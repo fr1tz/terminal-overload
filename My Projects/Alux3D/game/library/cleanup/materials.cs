e04c0b3bf17bd4f5aa67902cd824d7222c102fe6
Michael Goldener <mg@wasted.ch>
2013-06-20 23:18:23 +0200

    More work on "DM2" map.

--- My Projects/Alux3D/game/library/cleanup/materials.cs
+++ My Projects/Alux3D/game/library/cleanup/materials.cs
@@ -1,15 +1,23 @@
+singleton Material(TerrainFX_dm2_grid1)
+{
+   mapTo = "dm2_grid1";
+   footstepSoundId = 0;
+   terrainMaterials = "1";
+   ShowDust = "true";
+   showFootprints = "true";
+   materialTag0 = "Terrain";
+   effectColor[0] = "0.00784314 0.0313726 0.996078 1";
+   effectColor[1] = "0.0313726 0.996078 0.00784314 1";
+};
 
 new TerrainMaterial()
 {
-   diffuseMap = "library/texture/alux3d/dm2/metal";
-   internalName = "metal1";
+   internalName = "dm2_grid1";
+   diffuseMap = "library/texture/alux3d/dm2/grid1";
    diffuseSize = "4";
-   useSideProjection = "0";
-   macroSize = "16";
-   macroDistance = "5";
-   macroStrength = "10";
-   detailMap = "library/texture/alux3d/dm2/metal";
+   detailMap = "library/texture/alux3d/dm2/grid1";
    detailSize = "4";
-   normalMap = "library/texture/alux3d/dm2/metal_n";
-   parallaxScale = "0";
+   detailDistance = "500";
 };
+
+
