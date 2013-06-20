8b2487233df0fd51b676268168e9332fd44897bc
Michael Goldener <mg@wasted.ch>
2013-06-20 20:23:41 +0200

    Add "dm2" map.

---
+++ My Projects/Alux3D/game/library/cleanup/materials.cs
@@ -0,0 +1,15 @@
+
+new TerrainMaterial()
+{
+   diffuseMap = "library/texture/alux3d/dm2/metal";
+   internalName = "metal1";
+   diffuseSize = "4";
+   useSideProjection = "0";
+   macroSize = "16";
+   macroDistance = "5";
+   macroStrength = "10";
+   detailMap = "library/texture/alux3d/dm2/metal";
+   detailSize = "4";
+   normalMap = "library/texture/alux3d/dm2/metal_n";
+   parallaxScale = "0";
+};
