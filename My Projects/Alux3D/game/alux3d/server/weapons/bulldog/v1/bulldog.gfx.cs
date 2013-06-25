2143a28b4aefb5a992ce20beccdc13a44d352930
Michael Goldener <mg@wasted.ch>
2013-06-25 09:29:44 +0200

    Tweak v1 Bulldog projectile particles.

--- My Projects/Alux3D/game/alux3d/server/weapons/bulldog/v1/bulldog.gfx.cs
+++ My Projects/Alux3D/game/alux3d/server/weapons/bulldog/v1/bulldog.gfx.cs
@@ -3,11 +3,11 @@
 
 datablock ParticleData(WpnBulldogProjectileParticles : DefaultParticle)
 {
-   sizes[0] = "0.0946103";
-   sizes[1] = "0.497467";
+   sizes[0] = "0.2";
+   sizes[1] = "0.05";
    sizes[2] = "0";
    sizes[3] = "0";
-   times[1] = "1";
+   times[1] = "0.5";
    times[2] = "1";
    inheritedVelFactor = "0";
    lifetimeMS = "1000";
@@ -16,14 +16,15 @@ datablock ParticleData(WpnBulldogProjectileParticles : DefaultParticle)
    spinSpeed = "0";
    textureName = "library/texture/alux3d/ring.png";
    animTexName = "library/texture/alux3d/ring.png";
-   colors[1] = "0.992126 0.0787402 0.00787402 0";
-   colors[2] = "0.996078 0.0784314 0.00784314 0.330709";
+   colors[1] = "0.988235 0.0862745 0.0156863 0.552";
+   colors[2] = "0.996078 0.0784314 0.00784314 0";
    colors[3] = "1 0.0705882 0 0";
    ejectionPeriodMS = "2";
    ejectionVelocity = "0";
    softnessDistance = "1";
    particles = "WpnBulldogProjectileParticle";
    ejectionOffset = "0";
+   colors[0] = "0.996078 0.992157 0.992157 1";
 };
 
 datablock ParticleEmitterData(WpnBulldogProjectileEmitter : DefaultEmitter)
@@ -32,10 +33,11 @@ datablock ParticleEmitterData(WpnBulldogProjectileEmitter : DefaultEmitter)
    ejectionPeriodMS = "2";
    periodVarianceMS = "0";
    softnessDistance = "1";
-   ejectionVelocity = "1";
+   ejectionVelocity = "0.5";
    ejectionOffset = "0";
-   thetaMin = "180";
+   thetaMin = "90";
    thetaMax = "90";
+   ambientFactor = "0";
 };
 
 datablock ParticleData(WpnBulldogProjectileExplosionPlasmaParticles)
