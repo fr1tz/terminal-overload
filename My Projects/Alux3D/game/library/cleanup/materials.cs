25385d6b45e2706a2a2942e1f8b6e6bf41a301ed
Michael Goldener <mg@wasted.ch>
2013-06-27 13:06:59 +0200

    More work on "DM2" map.

--- My Projects/Alux3D/game/library/cleanup/materials.cs
+++ My Projects/Alux3D/game/library/cleanup/materials.cs
@@ -1,24 +1,3 @@
-singleton Material(TerrainFX_dm2_grid1)
-{
-   mapTo = "dm2_grid1";
-   footstepSoundId = 0;
-   terrainMaterials = "1";
-   ShowDust = "true";
-   showFootprints = "true";
-   materialTag0 = "Terrain";
-   effectColor[0] = "0.00784314 0.0313726 0.996078 1";
-   effectColor[1] = "0.0313726 0.996078 0.00784314 1";
-};
-
-new TerrainMaterial()
-{
-   internalName = "dm2_grid1";
-   diffuseMap = "library/texture/alux3d/dm2/grid1";
-   diffuseSize = "4";
-   detailMap = "library/texture/alux3d/dm2/grid1";
-   detailSize = "4";
-   detailDistance = "500";
-   normalMap = "library/texture/alux3d/dm2/grid1_n";
-};
-
+// Copyright information can be found in the file named COPYING
+// located in the root directory of this distribution.
 
