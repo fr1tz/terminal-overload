856d0dd8763ffd1025c39d3b809903d3ba05bc13
Michael Goldener <mg@wasted.ch>
2013-07-04 18:02:29 +0200

    Add p2 of Raptor image shape.

---
+++ My Projects/Alux3D/game/library/shape/alux3d/raptor/image/p2/materials.cs
@@ -0,0 +1,29 @@
+
+
+singleton Material(shape_fp_shape_alux3d_raptor_image_p2_mat1)
+{
+   mapTo = "shape_alux3d_raptor_image_p2_mat1";
+   diffuseColor[0] = "0.992157 0.996078 0.992157 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "21";
+   doubleSided = "0";
+   translucentBlendOp = "None";
+   glow[0] = "0";
+   emissive[0] = "0";
+   pixelSpecular[0] = "1";
+   materialTag0 = "Miscellaneous";
+   scrollDir[0] = "1 1";
+   scrollSpeed[0] = "0.118";
+   waveFreq[0] = "2.656";
+   waveAmp[0] = "0.891";
+};
+
+singleton Material(shape_fp_shape_alux3d_raptor_image_p2_mat2)
+{
+   mapTo = "shape_alux3d_raptor_image_p2_mat2";
+   diffuseColor[0] = "0.110067 0.047326 0.0247067 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
