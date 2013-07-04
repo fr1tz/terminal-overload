12254e81bbd135069c3552a7ea74b3529975417a
Michael Goldener <mg@wasted.ch>
2013-07-04 18:02:59 +0200

    Add missing files.

---
+++ My Projects/Alux3D/game/library/shape/alux3d/light/p1/materials.cs
@@ -0,0 +1,15 @@
+
+
+singleton Material(shape_alux3d_light_p1_mat1)
+{
+   mapTo = "shape_alux3d_light_p1_mat1";
+   diffuseColor[0] = "0.64 0.0494712 0.567496 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "Add";
+   glow[0] = "1";
+   emissive[0] = "1";
+   translucent = "1";
+   castShadows = "0";
+};
