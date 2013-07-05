1432282426f720dbdab0f40deb272ab1b344bcea
Michael Goldener <mg@wasted.ch>
2013-07-05 13:28:16 +0200

    More work on p2 Raptor image shape.

--- My Projects/Alux3D/game/library/shape/alux3d/raptor/image/p2/materials.cs
+++ My Projects/Alux3D/game/library/shape/alux3d/raptor/image/p2/materials.cs
@@ -3,28 +3,32 @@
 singleton Material(shape_fp_shape_alux3d_raptor_image_p2_mat1)
 {
    mapTo = "shape_alux3d_raptor_image_p2_mat1";
-   diffuseColor[0] = "0.992157 0.996078 0.992157 1";
-   specular[0] = "0.5 0.5 0.5 1";
+   diffuseColor[0] = "0.466667 0.466667 0.466667 1";
+   specular[0] = "0.996078 0.996078 0.996078 1";
    specularPower[0] = "1";
    doubleSided = "0";
    translucentBlendOp = "None";
    glow[0] = "0";
    emissive[0] = "0";
-   pixelSpecular[0] = "0";
+   pixelSpecular[0] = "1";
    materialTag0 = "Miscellaneous";
    scrollDir[0] = "1 1";
    scrollSpeed[0] = "0.118";
    waveFreq[0] = "2.656";
    waveAmp[0] = "0.891";
-   cubemap = "nightCubemap";
+   diffuseMap[0] = "library/shape/alux3d/raptor/image/p2/noise.png";
+   rotSpeed[0] = "-0.12";
+   rotPivotOffset[0] = "-0.5 -0.5";
+   detailScale[0] = "1 1";
+   normalMap[0] = "library/shape/alux3d/raptor/image/p2/grid_n.png";
 };
 
 singleton Material(shape_fp_shape_alux3d_raptor_image_p2_mat2)
 {
    mapTo = "shape_alux3d_raptor_image_p2_mat2";
-   diffuseColor[0] = "0.110067 0.047326 0.0247067 1";
+   diffuseColor[0] = "0.996078 0.898039 0.733333 1";
    specular[0] = "0.5 0.5 0.5 1";
-   specularPower[0] = "6";
+   specularPower[0] = "1";
    doubleSided = "1";
    translucentBlendOp = "None";
    glow[0] = "1";
