703a412988253f4321f3042efa371decc607359f
Michael Goldener <mg@wasted.ch>
2013-07-10 17:21:48 +0200

    Add p3 of raptor image shape.

---
+++ My Projects/Alux3D/game/library/shape/alux3d/raptor/image/p3/materials.cs
@@ -0,0 +1,22 @@
+
+singleton Material(shape_fp_shape_alux3d_raptor_image_p3_mat1)
+{
+   mapTo = "shape_alux3d_raptor_image_p3_mat1";
+   diffuseMap[0] = "library/shape/alux3d/raptor/image/p3/base.png";
+   normalMap[0] = "library/shape/alux3d/raptor/image/p3/normal.png";
+   glow[1] = "0";
+   emissive[1] = "0";
+   materialTag0 = "Miscellaneous";
+};
+
+singleton Material(shape_fp_shape_alux3d_raptor_image_p3_mat2)
+{
+   mapTo = "shape_alux3d_raptor_image_p3_mat2";
+   diffuseColor[0] = "0.996078 0.898039 0.733333 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   glow[0] = "1";
+   materialTag0 = "Miscellaneous";
+};
