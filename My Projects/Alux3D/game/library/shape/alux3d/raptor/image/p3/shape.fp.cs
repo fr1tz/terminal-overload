89d14143858a120b9746018dcf4cb14c19f8ea8f
Michael Goldener <mg@wasted.ch>
2013-07-10 22:06:31 +0200

    More work on p3 raptor image shape.

--- My Projects/Alux3D/game/library/shape/alux3d/raptor/image/p3/shape.fp.cs
+++ My Projects/Alux3D/game/library/shape/alux3d/raptor/image/p3/shape.fp.cs
@@ -8,8 +8,11 @@ function Shape_Alux3D_Raptor_Image_P3_FP::onLoad(%this)
 {
    %this.renameSequence("ambient", "timeline");
    %this.addSequence("timeline", "root", "25", "35", "1", "0");
-   %this.addSequence("timeline", "idle", "25", "35", "1", "0");
-   %this.addSequence("timeline", "fire", "40", "50", "1", "0");
-   %this.addSequence("timeline", "reload", "80", "163", "1", "0");
+   %this.addSequence("timeline", "ready", "25", "35", "1", "0");
+   %this.addSequence("timeline", "empty", "60", "70", "1", "0");
+   %this.addSequence("timeline", "ready_moving", "190", "230", "1", "0");
+   %this.addSequence("timeline", "empty_moving", "260", "300", "1", "0");
+   %this.addSequence("timeline", "fire", "350", "360", "1", "0");
+   %this.addSequence("timeline", "reload", "500", "583", "1", "0");
    %this.setSequenceCyclic("fire", "0");
 }
