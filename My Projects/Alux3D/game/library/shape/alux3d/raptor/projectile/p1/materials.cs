b8a027ee62eafd5e5af34f9858327f1e889ef910
Michael Goldener <mg@wasted.ch>
2013-07-11 17:20:59 +0200

    More work on p1 raptor projectile shape.

--- My Projects/Alux3D/game/library/shape/alux3d/raptor/projectile/p1/materials.cs
+++ My Projects/Alux3D/game/library/shape/alux3d/raptor/projectile/p1/materials.cs
@@ -4,11 +4,22 @@ singleton Material(shape_alux3d_raptor_projectile_p1_mat1)
    mapTo = "shape_alux3d_raptor_projectile_p1_mat1";
    diffuseColor[0] = "1 0.792157 0 1";
    doubleSided = "0";
-   translucentBlendOp = "Add";
+   translucentBlendOp = "AddAlpha";
    glow[0] = "1";
    emissive[0] = "1";
    translucent = "1";
    castShadows = "0";
    parallaxScale[0] = "0.2";
    materialTag0 = "Miscellaneous";
+};
+
+singleton Material(shape_alux3d_raptor_projectile_p1_mat2)
+{
+   mapTo = "shape_alux3d_raptor_projectile_p1_mat2";
+   diffuseColor[0] = "0.996078 0.996078 0.996078 0.27";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   translucent = "1";
+   glow[0] = "1";
+   emissive[0] = "1";
 };
