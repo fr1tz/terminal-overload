a509d624cdff7ad55d2f23a56387377504e84fca
Michael Goldener <mg@wasted.ch>
2013-09-27 20:14:24 +0200

    Add missing content files for "siberion" rifle.

---
+++ My Projects/Alux3D/game/content/alux3d/release1/shapes/siberion/image/p1/materials.cs
@@ -0,0 +1,26 @@
+
+singleton Material(alux3d_release1_shape_siberion_image_p1_mat1)
+{
+   mapTo = "alux3d_release1_shape_siberion_image_p1_mat1";
+   diffuseMap[0] = "content/alux3d/release1/shapes/raptor/image/p3/base.png";
+   normalMap[0] = "content/alux3d/release1/shapes/raptor/image/p3/normal.png";
+   glow[1] = "0";
+   emissive[1] = "0";
+   materialTag0 = "Miscellaneous";
+};
+
+singleton Material(alux3d_release1_shape_siberion_image_p1_mat3)
+{
+   mapTo = "alux3d_release1_shape_siberion_image_p1_mat3";
+   diffuseColor[0] = "0.992157 0.996078 0.996078 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucent = "1";
+   diffuseMap[0] = "content/alux3d/release1/shapes/raptor/image/p3/muzzleflash.png";
+   glow[0] = "1";
+   emissive[0] = "1";
+   castShadows = "0";
+   translucentBlendOp = "Add";
+   materialTag0 = "Miscellaneous";
+};
---
+++ My Projects/Alux3D/game/content/alux3d/release1/shapes/siberion/projectile/p1/materials.cs
@@ -0,0 +1,21 @@
+
+singleton Material(alux3d_release1_shapes_siberion_projectile_p1_mat1)
+{
+   mapTo = "alux3d_release1_shapes_siberion_projectile_p1_mat1";
+   diffuseColor[0] = "0.64 0.456948 0 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(shape_alux3d_release1_shapes_siberion_projectile_p1_mat1)
+{
+   mapTo = "alux3d_release1_shapes_siberion_projectile_p1_mat1";
+   diffuseColor[0] = "1 0.929412 0 0.15";
+   glow[0] = "1";
+   emissive[0] = "1";
+   castShadows = "0";
+   translucent = "1";
+   translucentBlendOp = "AddAlpha";
+   materialTag0 = "Miscellaneous";
+   showFootprints = "0";
+};
