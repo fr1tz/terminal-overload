6d3d36d1cbcd2fca6f59f8eb1a7a101a36eeef94
Michael Goldener <mg@wasted.ch>
2013-09-27 19:34:47 +0200

    Development work on "frontline4" mission.

---
+++ My Projects/Alux3D/game/content/fr1tz/mis_alux3d_frontline4/materials.cs
@@ -0,0 +1,9 @@
+// Copyright information can be found in the file named COPYING
+// located in the root directory of this distribution.
+
+new TerrainMaterial()
+{
+   diffuseMap = "content/torque3d/3.0/terrains/Example/sand";
+   detailMap = "content/torque3d/3.0/terrains/Example/sand_d";
+   internalName = "fr1tz_mis_alux3d_frontline4_sand";
+};
