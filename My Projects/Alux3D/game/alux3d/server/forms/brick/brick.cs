44c246697ec31943a35852f8c6543211331c7bf6
Michael Goldener <mg@wasted.ch>
2013-11-05 00:27:27 +0100

    Try to improve BrickVolume::destroy().

--- My Projects/Alux3D/game/alux3d/server/forms/brick/brick.cs
+++ My Projects/Alux3D/game/alux3d/server/forms/brick/brick.cs
@@ -19,19 +19,25 @@ datablock HexagonVolumeData(BrickVolume)
 
 function BrickVolume::destroyStack(%this, %volume, %gridPos2D, %pos, %radius)
 {
+   //echo("BrickVolume::destroyStack()");
    %shapeNr = %volume.getHexagonShapeNr(%gridPos2D);
    %elevation = %volume.getHexagonElevation(%gridPos2D);
    %amount = %volume.getHexagonAmount(%gridPos2D);
+   //echo(" shapeNr:" SPC %shapeNr SPC "elevation:" SPC %elevation SPC "amount:" SPC %amount);
    for(%i = 0; %i < %amount; %i++)
    {
       %gridPosZ = %elevation + %i;
       %gridPos = %gridPos2D SPC %gridPosZ;
       %worldPos = MissionSoilGrid.gridToWorld(%gridPos);
       %dist = VectorLen(VectorSub(%worldPos, %pos));
+      //echo(" dist:" SPC %dist);
       if(%dist <= %radius)
       {
          %gridPos = %gridPos2D SPC %elevation;
          %newAmount = %gridPosZ - %elevation;
+         //echo(" newAmount:" SPC %newAmount);
+         if(%newAmount <= 0)
+            %shapeNr = 0;
          %volume.setHexagon(%gridPos, %shapeNr, %newAmount);
          %volume.zNumBricks -= (%amount - %newAmount);
          break;
@@ -39,8 +45,10 @@ function BrickVolume::destroyStack(%this, %volume, %gridPos2D, %pos, %radius)
    }
 }
 
-function BrickVolume::destroy(%this, %volume, %pos, %radius)
+function BrickVolume::destroy(%this, %volume, %pos, %radius, %spareShapeNr)
 {
+   //echo("BrickVolume::destroy()");
+   //echo("numBricks start:" SPC %volume.zNumBricks);
    %numBricksStor = %volume.zNumBricks;
 
    %vScale = %volume.getScale();
@@ -55,8 +63,12 @@ function BrickVolume::destroy(%this, %volume, %pos, %radius)
    %x = %vPosX - %vScaleX/2;
    %y = %vPosY - %vScaleY/2;
    %gridPos = MissionSoilGrid.worldToGrid(%x SPC %y SPC %vPosZ);
-   %gridPosX = getWord(%gridPos, 0)+1;
-   %gridPosY = getWord(%gridPos, 1)+1;
+   %gridPosX = getWord(%gridPos, 0);
+   %gridPosY = getWord(%gridPos, 1);
+   if(%volume.getHexagonShapeNr(%gridPosX SPC %gridPosY) == -1)
+      %gridPosX++;
+   if(%volume.getHexagonShapeNr(%gridPosX SPC %gridPosY) == -1)
+      %gridPosY++;
 
    %x = 0;
    %y = 0;
@@ -77,13 +89,15 @@ function BrickVolume::destroy(%this, %volume, %pos, %radius)
             continue;
          }
       }
-      else if(%shapeNr > 0)
+      else if(%shapeNr > 0 && %shapeNr != %spareShapeNr)
       {
          %this.destroyStack(%volume, %gridPos2D, %pos, %radius);
       }
       
       %x++;
    }
+
+   //echo("numBricks end:" SPC %volume.zNumBricks);
    
    if(%volume.zNumBricks == 0)
       %volume.delete();
