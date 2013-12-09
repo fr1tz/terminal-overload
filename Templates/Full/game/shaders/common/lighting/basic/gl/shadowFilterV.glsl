ac255014dabe1140350233143e068918dfde75c4
LuisAntonRebollo <souga_el_diablo_negro@hotmail.com>
2013-12-09 02:47:12 +0100

    OpenGL: PostEffect( BL_ShadowFilterPostFx ) check HLSL and port to OpenGL/GLSL

--- Templates/Full/game/shaders/common/lighting/basic/gl/shadowFilterV.glsl
+++ Templates/Full/game/shaders/common/lighting/basic/gl/shadowFilterV.glsl
@@ -22,13 +22,13 @@
 
 #include "../../../../../../shaders/common/gl/torque.glsl"
 
-uniform vec2 oneOverTargetSize;
 uniform vec4 rtParams0;
 
 varying vec2 uv;
 
 void main()
 {
-	gl_Position = gl_Vertex;   
+   gl_Position = gl_Vertex;   
    uv = viewportCoordToRenderTarget( gl_MultiTexCoord0.st, rtParams0 ); 
+   gl_Position.y *= -1; //correct ssp
 }
