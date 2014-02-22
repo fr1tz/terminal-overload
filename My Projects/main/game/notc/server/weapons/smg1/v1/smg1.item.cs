40f620b5bd447d69c3097f78c0051c6969880400
Michael Goldener <mg@wasted.ch>
2014-02-22 22:12:54 +0100

    Remove unnecessary items for SMG1-v1 and MGL1-v1.

--- My Projects/main/game/notc/server/weapons/smg1/v1/smg1.item.cs
+++ My Projects/main/game/notc/server/weapons/smg1/v1/smg1.item.cs
@@ -1,45 +1,6 @@
 // Copyright information can be found in the file named COPYING
 // located in the root directory of this distribution.
 
-datablock ItemData(WpnSMG1Ammo)
-{
-   // Mission editor category
-   category = "Ammo";
-
-   // Add the Ammo namespace as a parent.  The ammo namespace provides
-   // common ammo related functions and hooks into the inventory system.
-   className = "Ammo";
-
-   // Basic Item properties
-   shapeFile = "content/fr1tz/oldshapes/raptor/magazine/p1/shape.dae";
-   mass = 1;
-   elasticity = 0.2;
-   friction = 0.6;
-
-   // Dynamic properties defined by the scripts
-   pickUpName = "Raptor Rounds";
-   count = 30;
-};
-
-datablock ItemData(WpnSMG1Clip)
-{
-   // Mission editor category
-   category = "AmmoClip";
-
-   // Add the Ammo namespace as a parent.  The ammo namespace provides
-   // common ammo related functions and hooks into the inventory system.
-   className = "AmmoClip";
-
-   // Basic Item properties
-   shapeFile = "content/fr1tz/oldshapes/raptor/magazine/p1/shape.dae";
-   mass = 1;
-   elasticity = 0.2;
-   friction = 0.6;
-
-   // Dynamic properties defined by the scripts
-   pickUpName = "Raptor Magazine";
-};
-
 datablock ItemData(WpnSMG1)
 {
    // Mission editor category
