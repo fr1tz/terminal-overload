168eb80add5205b68a765f54163be27555d961f2
Michael Goldener <mg@wasted.ch>
2014-02-23 17:09:31 +0100

    Add decal material for p1 SG1 projectile.

---
+++ My Projects/main/game/content/fr1tz/notc1/shapes/sg1/projectile/p1/materials.cs
@@ -0,0 +1,16 @@
+
+singleton Material(fr1tz_notc1_shapes_sg1_projectile_p1_decalmat)
+{
+   mapTo = "unmapped_mat";
+   diffuseMap[0] = "content/rotc/p.5.4/textures/rotc/corona.png";
+   translucent = "1";
+   translucentBlendOp = "AddAlpha";
+   castShadows = "0";
+   showFootprints = "0";
+   glow[0] = "1";
+   emissive[0] = "1";
+   diffuseColor[0] = "1 0.929412 0 1";
+   translucentZWrite = "1";
+   vertColor[0] = "1";
+};
+
