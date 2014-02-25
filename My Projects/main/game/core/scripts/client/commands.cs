3394afa577d16f4bca7a8b841793496b7e542994
Michael Goldener <mg@wasted.ch>
2014-02-25 17:37:15 +0100

    Add clientCmdExecContentScript() function.

--- My Projects/main/game/core/scripts/client/commands.cs
+++ My Projects/main/game/core/scripts/client/commands.cs
@@ -7,3 +7,17 @@ function clientCmdSyncEditorGui()
    if (isObject(EditorGui))
       EditorGui.syncCameraGui();
 }
+
+function clientCmdExecContentScript(%path)
+{
+   if(strstr(%path, ".." ) != -1)
+   {
+      echo("clientCmdExecContentScript(): skipping" SPC %path);
+      return;
+   }
+   %stor = $instantGroup;
+   $instantGroup = ClientMissionCleanup;
+   exec("content/" @ %path);
+   $instantGroup = %stor;
+}
+
