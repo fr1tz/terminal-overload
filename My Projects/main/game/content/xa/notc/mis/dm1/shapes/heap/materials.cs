df7aaceed36a8f3db0f8fce459160d5674a41042
Michael Goldener <mg@wasted.ch>
2014-02-28 22:28:17 +0100

    Add DM1 map heap tube shape.

---
+++ My Projects/main/game/content/xa/notc/mis/dm1/shapes/heap/materials.cs
@@ -0,0 +1,41 @@
+
+singleton Material(xa_notc_mis_dm1_shapes_heap_tube_mat1)
+{
+   mapTo = "xa_notc_mis_dm1_shapes_heap_tube_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.992157 1";
+   doubleSided = "0";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "content/xa/notc/core/textures/grid.128.4.h1.png";
+   glow[0] = "1";
+   emissive[0] = "1";
+   translucent = "0";
+   castShadows = "0";
+   showFootprints = "0";
+   specularStrength[0] = "0.25";
+};
+
+singleton Material(xa_notc_mis_dm1_shapes_heap_tube_mat2)
+{
+   mapTo = "xa_notc_mis_dm1_shapes_heap_tube_mat2";
+   diffuseColor[0] = "0.627932 0.630949 0.64 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "content/xa/notc/core/textures/hex.png";
+};
+
+singleton Material(xa_notc_mis_dm1_shapes_heap_tube_mat0)
+{
+   mapTo = "xa_notc_mis_dm1_shapes_heap_tube_mat0";
+   diffuseColor[0] = "0.996078 0.992157 0.996078 1";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   diffuseMap[0] = "content/xa/notc/core/shapes/mgl1/image/p1/fx2.png";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   animFlags[0] = "0x00000001";
+   scrollDir[0] = "1 0.069";
+   scrollSpeed[0] = "10";
+   glow[0] = "1";
+   emissive[0] = "1";
+};
