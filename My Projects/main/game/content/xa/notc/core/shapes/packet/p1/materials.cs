664023362ac3a696280889873eea366f79c370d7
Michael Goldener <mg@wasted.ch>
2014-03-02 02:03:04 +0100

    Add p1 packet shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/packet/p1/materials.cs
@@ -0,0 +1,54 @@
+
+singleton Material(xa_notc_core_shapes_packet_p1_mat2)
+{
+   mapTo = "xa_notc_core_shapes_packet_p1_mat2";
+   diffuseColor[0] = "0.00392157 0.00392157 0.00392157 0";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   glow[0] = "1";
+   emissive[0] = "1";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   showDust = "1";
+};
+
+singleton Material(xa_notc_core_shapes_packet_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_packet_p1_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.992157 1";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   parallaxScale[0] = "0.25";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   glow[0] = "1";
+   emissive[0] = "1";
+};
+
+singleton Material(xa_notc_core_shapes_packet_p1_mat3)
+{
+   mapTo = "xa_notc_core_shapes_packet_p1_mat3";
+   diffuseColor[0] = "0.996078 0.807843 0.00784314 0.552";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   glow[0] = "1";
+   emissive[0] = "1";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+};
+
+singleton Material(xa_notc_core_shapes_packet_p1_mat0)
+{
+   mapTo = "xa_notc_core_shapes_packet_p1_mat0";
+   diffuseColor[0] = "0.996078 0.94902 0.00784314 1";
+   doubleSided = "0";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "content/xa/notc/core/textures/grid.128.8.png";
+   glow[0] = "1";
+   emissive[0] = "1";
+   castShadows = "0";
+   showFootprints = "0";
+};
---
+++ My Projects/main/game/content/xa/notc/core/shapes/packet/p1/shape.cs
@@ -0,0 +1,6 @@
+
+singleton TSShapeConstructor(xa_notc_core_shapes_packet_p1_dts)
+{
+   baseShape = "./shape.dae";
+   loadLights = "0";
+};
