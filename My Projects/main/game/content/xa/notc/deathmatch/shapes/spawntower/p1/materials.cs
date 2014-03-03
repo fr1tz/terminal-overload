4590d646974e2cef9d5f54692bb134ee871cfbdd
Michael Goldener <mg@wasted.ch>
2014-03-03 03:59:19 +0100

    Add p1 deathmatch spawn tower shape to xa.

---
+++ My Projects/main/game/content/xa/notc/deathmatch/shapes/spawntower/p1/materials.cs
@@ -0,0 +1,29 @@
+
+singleton Material(shape_xa_notc_deathmatch_shapes_spawntower_p1_mat1)
+{
+   mapTo = "xa_notc_deathmatch_shapes_spawntower_p1_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.992157 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "content/xa/notc/core/textures/grid.128.4.h1.png";
+   glow[0] = "1";
+   emissive[0] = "1";
+};
+
+singleton Material(shape_xa_notc_deathmatch_shapes_spawntower_p1_mat0)
+{
+   mapTo = "xa_notc_deathmatch_shapes_spawntower_p1_mat0";
+   diffuseColor[0] = "0.992157 0.996078 0.992157 1";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   diffuseMap[0] = "content/xa/notc/core/shapes/mgl1/image/p1/fx2.png";
+   glow[0] = "1";
+   emissive[0] = "1";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   specularPower[0] = "1";
+   animFlags[0] = "0x00000001";
+   scrollDir[0] = "1 0.172";
+   scrollSpeed[0] = "5";
+};
---
+++ My Projects/main/game/content/xa/notc/deathmatch/shapes/spawntower/p1/shape.cs
@@ -0,0 +1,6 @@
+
+singleton TSShapeConstructor(shape_xa_notc_deathmatch_shapes_spawntower_p1_dts)
+{
+   baseShape = "./shape.dae";
+   loadLights = "0";
+};
