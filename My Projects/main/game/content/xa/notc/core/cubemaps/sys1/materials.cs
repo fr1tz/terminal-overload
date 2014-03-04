325a340e6f3a8fb1edfef62f46f8ab2d615b4d90
Michael Goldener <mg@wasted.ch>
2014-03-04 06:48:59 +0100

    Add sys1 cubemap to xa.

---
+++ My Projects/main/game/content/xa/notc/core/cubemaps/sys1/materials.cs
@@ -0,0 +1,17 @@
+
+new CubemapData(xa_notc_core_cubemaps_sys1)
+{
+   cubeFace[0] = "content/xa/notc/core/cubemaps/sys1/top.png";
+   cubeFace[1] = "content/xa/notc/core/cubemaps/sys1/top.png";
+   cubeFace[2] = "content/xa/notc/core/cubemaps/sys1/top.png";
+   cubeFace[3] = "content/xa/notc/core/cubemaps/sys1/top.png";
+   cubeFace[4] = "content/xa/notc/core/cubemaps/sys1/top.png";
+   cubeFace[5] = "content/xa/notc/core/cubemaps/sys1/top.png";
+};
+
+singleton Material(xa_notc_core_cubemaps_sys1_mat)
+{
+   cubemap = "xa_notc_core_cubemaps_sys1";
+   materialTag0 = "Skies";
+};
+
