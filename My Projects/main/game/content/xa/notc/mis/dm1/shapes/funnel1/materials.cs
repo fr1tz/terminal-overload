5c86afa28ae24c364244134b5f7239978f6e7b7f
Michael Goldener <mg@wasted.ch>
2014-03-06 01:23:12 +0100

    Add alt version of DM1 map.

---
+++ My Projects/main/game/content/xa/notc/mis/dm1/shapes/funnel1/materials.cs
@@ -0,0 +1,13 @@
+
+singleton Material(shape_xa_notc_mis_dm1_shapes_funnel1_mat1)
+{
+   mapTo = "xa_notc_mis_dm1_shapes_funnel1_mat1";
+   diffuseColor[0] = "0.992157 0.996078 0.996078 1";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   diffuseMap[0] = "content/xa/notc/mis/dm1/shapes/funnel1/funnel1.png";
+   emissive[0] = "1";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+};
--- My Projects/main/game/content/xa/notc/mis/dm1/shapes/heap/materials.cs
+++ My Projects/main/game/content/xa/notc/mis/dm1/shapes/heap/materials.cs
@@ -2,18 +2,21 @@
 singleton Material(xa_notc_mis_dm1_shapes_heap_mat1)
 {
    mapTo = "xa_notc_mis_dm1_shapes_heap_mat1";
-   diffuseColor[0] = "0.00784314 0.627451 0.996078 1";
+   diffuseColor[0] = "0.992157 0.996078 0.996078 1";
    doubleSided = "1";
    translucentBlendOp = "AddAlpha";
-   diffuseMap[0] = "content/xa/notc/mis/dm1/shapes/heap/funnel1.png";
+   diffuseMap[0] = "content/xa/notc/core/textures/grid.128.4.png";
    glow[0] = "1";
    emissive[0] = "1";
-   animFlags[0] = "0x00000001";
+   animFlags[0] = "0x00000000";
    scrollDir[0] = "0 1";
    scrollSpeed[0] = "1";
    castShadows = "0";
    translucent = "1";
    showFootprints = "0";
+   diffuseColor[1] = "0.992157 0.996078 0.996078 0";
+   emissive[1] = "1";
+   materialTag0 = "Skies";
 };
 
 singleton Material(xa_notc_mis_dm1_shapes_heap_mat0)
