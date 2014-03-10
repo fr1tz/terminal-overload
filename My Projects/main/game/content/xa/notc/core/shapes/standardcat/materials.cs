f2bb32bc110119e4e8d6a3607e870730f19dd780
Michael Goldener <mg@wasted.ch>
2014-03-10 13:07:14 +0100

    Tweak standard CAT materials.

--- My Projects/main/game/content/xa/notc/core/shapes/standardcat/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/standardcat/materials.cs
@@ -12,7 +12,7 @@ new Material(xa_notc_core_shapes_standardcat_footprint)
    emissive[0] = "1";
    translucentBlendOp = "AddAlpha";
    showFootprints = "0";
-   diffuseColor[0] = "1 0.882353 0 1";
+   diffuseColor[0] = "1 1 1 1";
 };
 
 new Material(xa_notc_core_shapes_standardcat_slidedecalmat)
@@ -28,7 +28,7 @@ new Material(xa_notc_core_shapes_standardcat_slidedecalmat)
    emissive[0] = "1";
    translucentBlendOp = "AddAlpha";
    showFootprints = "0";
-   diffuseColor[0] = "0.996078 0.698039 0.00784314 1";
+   diffuseColor[0] = "1 1 1 1";
 };
 
 singleton Material(xa_notc_core_shapes_standardcat_skiddecalmat)
@@ -46,32 +46,35 @@ singleton Material(xa_notc_core_shapes_standardcat_skiddecalmat)
 singleton Material(xa_notc_core_shapes_standardcat_mat2)
 {
    mapTo = "armor_red0024";
-   glow[0] = "0";
-   glow[1] = "1";
-   emissive[0] = "0";
-   emissive[1] = "1";
    translucentBlendOp = "None";
    animFlags[1] = "0x00000000";
    scrollDir[1] = "0 0";
    scrollSpeed[1] = "0";
+   emissive[0] = "0";
+   glow[0] = "0";
    diffuseColor[0] = "0.00392157 0.00392157 0.00392157 1";
-   diffuseColor[1] = "0.988235 0.65098 0.0156863 1";
-   specularStrength[1] = "0";
    diffuseMap[0] = "content/xa/notc/core/textures/white.128.alpha25.png";
-   materialTag0 = "Miscellaneous";
    normalMap[0] = "content/xa/notc/core/textures/grid128.12.h2.n2.png";
    pixelSpecular[0] = "1";
-   diffuseColor[2] = "0.00392157 0.00392157 0.00392157 0";
-   glow[2] = "1";
+   emissive[1] = "1";
+   glow[1] = "1";
+   diffuseColor[1] = "0.172549 0.172549 0.172549 1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.8.png";
+   diffuseColorPaletteSlot[1] = "0";
    emissive[2] = "1";
-   diffuseColor[3] = "0.00392157 0.00392157 0.00392157 0";
+   glow[2] = "1";
+   diffuseColor[2] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[2] = "0";
+   wireframe[2] = "1";
+   specularStrength[1] = "0";
+   materialTag0 = "Miscellaneous";
    doubleSided = "1";
    translucent = "0";
    translucentZWrite = "1";
    alphaRef = "0";
    showFootprints = "0";
    cubemap = "xa_notc_core_cubemaps_chrome1";
-   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.8.png";
+   diffuseMap[2] = "content/xa/notc/core/textures/grid128.12.png";
 };
 
 singleton Material(xa_notc_core_shapes_standardcat_mat1)
