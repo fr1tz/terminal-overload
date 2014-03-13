d0d31e3aeceb9406b28451e8632ec16c8d1f0110
LuisAntonRebollo <souga_el_diablo_negro@hotmail.com>
2014-03-13 12:35:57 +0100

    OpenGL: ShaderGen MFT_IsTranslucentZWrite

--- Engine/source/lighting/advanced/glsl/gBufferConditionerGLSL.cpp
+++ Engine/source/lighting/advanced/glsl/gBufferConditionerGLSL.cpp
@@ -164,7 +164,7 @@ void GBufferConditionerGLSL::processPix(  Vector<ShaderComponent*> &componentLis
    if ( fd.features[ MFT_IsTranslucentZWrite ] )
    {
       alphaVal = new Var( "outAlpha", "float" );
-      meta->addStatement( new GenOp( "   @ = OUT.col.a; // MFT_IsTranslucentZWrite\r\n", new DecOp( alphaVal ) ) );
+      meta->addStatement( new GenOp( "   @ = col.a; // MFT_IsTranslucentZWrite\r\n", new DecOp( alphaVal ) ) );
    }
 
    // If using interlaced normals, invert the normal
