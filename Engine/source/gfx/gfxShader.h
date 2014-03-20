6a36c659d2cdc23b203819e0caad4f1ae77343d0
LuisAntonRebollo <souga_el_diablo_negro@hotmail.com>
2014-03-20 18:32:57 +0100

    OpenGL: Review/clean engine/gfx.

--- Engine/source/gfx/gfxShader.h
+++ Engine/source/gfx/gfxShader.h
@@ -237,7 +237,10 @@ protected:
    /// The macros to be passed to the shader.      
    Vector<GFXShaderMacro> mMacros;
 
-   /// Ordered SamplerNames // TODO OPENGL REVIEW add doc
+   /// Ordered SamplerNames
+   /// We need to store a list of sampler for allow OpenGL to
+   /// assign correct location for each sampler.
+   /// GLSL 150 not allow explicit uniform location.   
    Vector<String> mSamplerNamesOrdered;
 
    /// The pixel version this is compiled for.
