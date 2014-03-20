42c04b86de58bc06c5218ed83fa182890950c715
Michael Goldener <mg@wasted.ch>
2014-03-20 17:50:10 +0100

    Work on p1 SMG1 image shape.

--- My Projects/main/game/content/xa/notc/core/shapes/smg1/image/p1/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/smg1/image/p1/materials.cs
@@ -2,44 +2,25 @@
 singleton Material(xa_notc_core_shapes_smg1_image_p1_mat1)
 {
    mapTo = "xa_notc_core_shapes_smg1_image_p1_mat1";
-   diffuseColor[0] = "0.00392157 0.00392157 0.00392157 1";
+   diffuseMap[0] = "content/xa/notc/core/textures/grid.128.4.h1.png";
+   diffuseColor[0] = "0.992157 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[0] = "1";
    specular[0] = "0.5 0.5 0.5 1";
    specularPower[0] = "50";
+   glow[0] = "1";
+   emissive[0] = "1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
+   diffuseColor[1] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[1] = "1";
+   wireframe[1] = "1";
+   emissive[1] = "1";
+   glow[1] = "1";
    doubleSided = "0";
    translucentBlendOp = "LerpAlpha";
-   diffuseColor[1] = "0.992157 0.992157 0.992157 1";
    castShadows = "0";
    translucent = "0";
    showFootprints = "0";
-   glow[0] = "0";
-   emissive[0] = "0";
-   diffuseColor[2] = "0.996078 0.996078 0.996078 1";
-   diffuseColorPaletteSlot[2] = "0";
-   diffuseColor[3] = "0.992157 0.996078 0.996078 0";
-   wireframe[3] = "0";
-   diffuseColorPaletteSlot[3] = "-1";
-   detailScale[1] = "1 1";
-   emissive[2] = "1";
-   emissive[3] = "1";
-   animFlags[1] = "0x00000008";
-   scrollDir[1] = "1 1";
-   scrollDir[2] = "1 1";
-   scrollSpeed[1] = "0.588";
-   scrollSpeed[2] = "0.941";
-   rotSpeed[1] = "1.765";
-   rotPivotOffset[1] = "-0.5 -0.4";
-   waveFreq[1] = "1.094";
-   waveAmp[1] = "1";
    materialTag0 = "Miscellaneous";
-   specularStrength[1] = "1.76471";
-   emissive[1] = "1";
-   specularStrength[2] = "1.75";
-   glow[2] = "1";
-   glow[1] = "1";
-   diffuseColorPaletteSlot[1] = "0";
-   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.4.png";
-   wireframe[2] = "1";
-   diffuseMap[2] = "content/xa/notc/core/textures/grid.128.4.png";
 };
 
 singleton Material(xa_notc_core_shapes_smg1_image_p1_mat2)
@@ -60,3 +41,5 @@ singleton Material(xa_notc_core_shapes_smg1_image_p1_mat2)
    translucentZWrite = "0";
    materialTag0 = "Miscellaneous";
 };
+
+
--- My Projects/main/game/content/xa/notc/core/shapes/smg1/image/p1/shape.tp.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/smg1/image/p1/shape.tp.cs
@@ -2,13 +2,14 @@
 singleton TSShapeConstructor(xa_notc_core_shapes_smg1_image_p1_tp_dts)
 {
    baseShape = "./shape.tp.dae";
+   lodType = "TrailingNumber";
+   loadLights = "0";
 };
 
 function xa_notc_core_shapes_smg1_image_p1_tp_dts::onLoad(%this)
 {
    %this.renameSequence("ambient", "timeline");
+   %this.setSequenceCyclic("timeline", "0");
    %this.addSequence("timeline", "idle", "0", "10", "1", "0");
-   %this.addSequence("timeline", "fire", "100", "110", "1", "0");
-   %this.setSequenceCyclic("fire", "0");
+   %this.addSequence("timeline", "fire", "50", "60", "1", "0");
 }
-
