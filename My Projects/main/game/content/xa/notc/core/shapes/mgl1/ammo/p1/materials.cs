0f00f56c884b3a3f1205fd45a97ea20a3d9f0e57
Michael Goldener <mg@wasted.ch>
2014-03-26 01:08:15 +0100

    Add p1 MGL1 ammo shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/mgl1/ammo/p1/materials.cs
@@ -0,0 +1,21 @@
+
+singleton Material(xa_notc_core_shapes_mgl1_ammo_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_mgl1_ammo_p1_mat1";
+   diffuseColor[0] = "0.352941 0.337255 0.0117647 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   glow[0] = "1";
+   emissive[0] = "1";
+   diffuseColor[1] = "0.996078 0.94902 0.494118 1";
+   diffuseMap[1] = "content/xa/notc/core/shapes/mgl1/ammo/p1/text.png";
+   emissive[1] = "1";
+   diffuseColor[2] = "0.992157 0.992157 0.992157 1";
+   diffuseMap[2] = "content/xa/notc/core/shapes/mgl1/ammo/p1/opaque.png";
+   specularPower[2] = "1";
+   specularStrength[2] = "4";
+   pixelSpecular[2] = "1";
+   animFlags[1] = "0x00000001";
+   scrollDir[1] = "0 1";
+   scrollSpeed[1] = "0.706";
+};
