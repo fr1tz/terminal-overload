f3e85437886b19a5599223c64af520a7a4d35d98
Michael Goldener <mg@wasted.ch>
2014-03-26 01:11:50 +0100

    Add WpnMGL1Ammo item.

--- My Projects/main/game/notc/server/weapons/mgl1/v1/mgl1.item.cs
+++ My Projects/main/game/notc/server/weapons/mgl1/v1/mgl1.item.cs
@@ -1,6 +1,27 @@
 // Copyright information can be found in the file named COPYING
 // located in the root directory of this distribution.
 
+datablock ItemData(WpnMGL1Ammo)
+{
+   // Mission editor category
+   category = "Ammo";
+
+   // Add the Ammo namespace as a parent.  The ammo namespace provides
+   // common ammo related functions and hooks into the inventory system.
+   className = "Ammo";
+
+   // Basic Item properties
+   shapeFile = "content/xa/notc/core/shapes/mgl1/ammo/p1/shape.dae";
+   mass = 1;
+   elasticity = 0.2;
+   friction = 0.6;
+
+   // Dynamic properties defined by the scripts
+   playAmbient = true;
+   pickUpName = "MGL1 Ammo";
+   count = 15;
+};
+
 datablock ItemData(WpnMGL1)
 {
    // Mission editor category
