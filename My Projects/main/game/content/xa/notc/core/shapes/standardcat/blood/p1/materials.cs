38433b38842d85c8b8ec4b11078fd26561ecb5e2
Michael Goldener <mg@wasted.ch>
2014-04-06 02:06:02 +0200

    Add p1 standard CAT blood shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/standardcat/blood/p1/materials.cs
@@ -0,0 +1,53 @@
+
+singleton Material(xa_notc_core_shapes_standardcat_blood_p1_decalmat)
+{
+   diffuseMap[0] = "content/xa/notc/core/shapes/standardcat/slidedecal.png";
+   vertColor[0] = true;
+   translucent = true;
+   castShadows = "0";
+   translucentZWrite = "1";
+   materialTag0 = "Player";
+   mapTo = "footprint";
+   glow[0] = "0";
+   emissive[0] = "1";
+   translucentBlendOp = "AddAlpha";
+   showFootprints = "0";
+   diffuseColor[0] = "1 1 1 1";
+};
+
+singleton Material(xa_notc_core_shapes_standardcat_blood_p1_mat2)
+{
+   mapTo = "xa_notc_core_shapes_standardcat_blood_p1_mat2";
+   diffuseColor[0] = "0.996078 0.709804 0.882353 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   glow[0] = "1";
+   emissive[0] = "1";
+   castShadows = "1";
+   showFootprints = "1";
+   wireframe[0] = "1";
+   translucent = "1";
+};
+
+singleton Material(xa_notc_core_shapes_standardcat_blood_p1_mat2)
+{
+   mapTo = "xa_notc_core_shapes_standardcat_blood_p1_mat2";
+   diffuseColor[0] = "0.996078 0.00784314 0.603922 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   glow[0] = "1";
+   emissive[0] = "1";
+   wireframe[0] = "1";
+};
+
+
+singleton Material(xa_notc_core_shapes_standardcat_blood_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_standardcat_blood_p1_mat1";
+   diffuseColor[0] = "0.996078 0.545098 0.85098 1";
+   glow[0] = "1";
+   emissive[0] = "1";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+};
