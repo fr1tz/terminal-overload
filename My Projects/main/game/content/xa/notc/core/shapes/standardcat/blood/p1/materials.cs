cff7568c3c2af4df80cfb404b6b2508f846f5695
Michael Goldener <mg@wasted.ch>
2014-04-06 06:24:48 +0200

    Tweak p1 standard CAT blood shape.

--- My Projects/main/game/content/xa/notc/core/shapes/standardcat/blood/p1/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/standardcat/blood/p1/materials.cs
@@ -2,7 +2,7 @@
 singleton Material(xa_notc_core_shapes_standardcat_blood_p1_decalmat)
 {
    diffuseMap[0] = "content/xa/notc/core/shapes/standardcat/slidedecal.png";
-   vertColor[0] = true;
+   vertColor[0] = "1";
    translucent = true;
    castShadows = "0";
    translucentZWrite = "1";
@@ -12,34 +12,8 @@ singleton Material(xa_notc_core_shapes_standardcat_blood_p1_decalmat)
    emissive[0] = "1";
    translucentBlendOp = "AddAlpha";
    showFootprints = "0";
-   diffuseColor[0] = "1 1 1 1";
+   diffuseColor[0] = "0.996078 0.635294 0.909804 1";
 };
-
-singleton Material(xa_notc_core_shapes_standardcat_blood_p1_mat2)
-{
-   mapTo = "xa_notc_core_shapes_standardcat_blood_p1_mat2";
-   diffuseColor[0] = "0.996078 0.709804 0.882353 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-   glow[0] = "1";
-   emissive[0] = "1";
-   castShadows = "1";
-   showFootprints = "1";
-   wireframe[0] = "1";
-   translucent = "1";
-};
-
-singleton Material(xa_notc_core_shapes_standardcat_blood_p1_mat2)
-{
-   mapTo = "xa_notc_core_shapes_standardcat_blood_p1_mat2";
-   diffuseColor[0] = "0.996078 0.00784314 0.603922 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-   glow[0] = "1";
-   emissive[0] = "1";
-   wireframe[0] = "1";
-};
-
 
 singleton Material(xa_notc_core_shapes_standardcat_blood_p1_mat1)
 {
@@ -47,7 +21,12 @@ singleton Material(xa_notc_core_shapes_standardcat_blood_p1_mat1)
    diffuseColor[0] = "0.996078 0.545098 0.85098 1";
    glow[0] = "1";
    emissive[0] = "1";
+   diffuseColor[1] = "0.996078 0.545098 0.85098 1";
+   glow[1] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
    castShadows = "0";
    translucent = "1";
    showFootprints = "0";
 };
+
