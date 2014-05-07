392821bd50b1bb44b7d743cfc98b40ac9322c042
Michael Goldener <mg@wasted.ch>
2014-05-07 22:07:38 +0200

    Add p1 launcher image shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/standardcat/launcher/p1/materials.cs
@@ -0,0 +1,31 @@
+
+singleton Material(xa_notc_core_shapes_standardcat_launcher_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_standardcat_launcher_p1_mat1";
+   emissive[0] = "0";
+   glow[0] = "0";
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
+   diffuseColorPaletteSlot[0] = "0";
+   diffuseMap[0] = "content/xa/notc/core/shapes/standardcat/armor.png";
+   normalMap[0] = "content/xa/notc/core/textures/grid128.12.h2.n2.png";
+   pixelSpecular[0] = "1";
+   emissive[1] = "1";
+   glow[1] = "1";
+   diffuseColor[1] = "0.992157 0.992157 0.992157 1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.4.png";
+   diffuseColorPaletteSlot[1] = "0";
+   diffuseColor[2] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[2] = "0";
+   diffuseMap[2] = "content/xa/notc/core/textures/grid128.12.png";
+   glow[2] = "1";
+   emissive[2] = "1";
+   wireframe[2] = "1";
+   materialTag0 = "Miscellaneous";
+   doubleSided = "0";
+   translucent = "0";
+   translucentZWrite = "0";
+   translucentBlendOp = "None";
+   alphaRef = "0";
+   showFootprints = "0";
+};
+
