e37bebc43b0574f5dddd519f590d0bd51f7ec8c8
Michael Goldener <mg@wasted.ch>
2014-05-08 11:29:52 +0200

    Tweak p1 RazorDisc projectile material.

--- My Projects/main/game/content/xa/notc/core/shapes/razordisc/projectile/p1/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/razordisc/projectile/p1/materials.cs
@@ -3,7 +3,6 @@ singleton Material(xa_notc_core_shapes_razordisc_projectile_p1_mat1)
 {
    mapTo = "xa_notc_core_shapes_razordisc_projectile_p1_mat1";
    diffuseColor[0] = "0.996078 0.996078 0.996078 1";
-   diffuseColorPaletteSlot[0] = "0";
    doubleSided = "1";
    translucentBlendOp = "AddAlpha";
    glow[0] = "1";
