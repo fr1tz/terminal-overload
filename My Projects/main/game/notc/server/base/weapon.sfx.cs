98f01d5449dfdd0634a0c67e495aca3ae737f986
Michael Goldener <mg@wasted.ch>
2014-05-09 21:06:59 +0200

    Add a couple generic weapon sound effect.

--- My Projects/main/game/notc/server/base/weapon.sfx.cs
+++ My Projects/main/game/notc/server/base/weapon.sfx.cs
@@ -1,6 +1,27 @@
 // Copyright information can be found in the file named COPYING
 // located in the root directory of this distribution.
 
+datablock SFXProfile(GenericNoDiscTargetSound)
+{
+   filename = "content/xa/rotc/p.5.4/sounds/rotc/denied1";
+   description = AudioGui;
+   preload = true;
+};
+
+datablock SFXProfile(GenericDiscLaunchSound)
+{
+   filename = "content/xa/rotc/p.5.4/sounds/rotc/throw1";
+   description = AudioClose3D;
+   preload = true;
+};
+
+datablock SFXProfile(GenericNoAmmoSound)
+{
+   filename = "content/xa/rotc/p.5.4/sounds/rotc/weaponEmpty";
+   description = AudioClose3D;
+   preload = true;
+};
+
 datablock SFXProfile(GenericMissEnemyEffect1Sound)
 {
    filename = "content/xa/rotc/p.5.4/sounds/rotc/flyby1";
