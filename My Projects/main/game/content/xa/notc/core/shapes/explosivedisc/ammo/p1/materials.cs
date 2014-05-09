623e2edf0732dc4f6e57493f5535a071d219ca87
Michael Goldener <mg@wasted.ch>
2014-05-09 22:52:07 +0200

    Add p1 Explosive Disc shapes.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/explosivedisc/ammo/p1/materials.cs
@@ -0,0 +1,15 @@
+
+singleton Material(xa_notc_core_shapes_explosivedisc_ammo_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_explosivedisc_ammo_p1_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.996078 1";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   wireframe[0] = "1";
+   emissive[0] = "1";
+   translucent = "1";
+   materialTag0 = "Miscellaneous";
+   castShadows = "0";
+   showFootprints = "0";
+};
+
