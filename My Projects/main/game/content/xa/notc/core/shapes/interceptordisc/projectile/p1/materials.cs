e0b319414f69a04a9c4fdbafd93300a7d3e45176
Michael Goldener <mg@wasted.ch>
2014-05-10 01:21:56 +0200

    Add p1 Interceptor Disc projectile shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/interceptordisc/projectile/p1/materials.cs
@@ -0,0 +1,27 @@
+
+singleton Material(xa_notc_core_shapes_interceptordisc_projectile_p1_decalmat)
+{
+   diffuseMap[0] = "content/xa/notc/core/textures/smashdecal2.png";
+   emissive[0] = "1";
+   translucent = "1";
+   translucentBlendOp = "AddAlpha";
+   castShadows = "0";
+   showFootprints = "0";
+   vertColor[0] = "1";
+};
+
+singleton Material(xa_notc_core_shapes_interceptordisc_projectile_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_interceptordisc_projectile_p1_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.996078 1";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   glow[0] = "1";
+   wireframe[0] = "1";
+   emissive[0] = "1";
+   translucent = "1";
+   materialTag0 = "Miscellaneous";
+   castShadows = "0";
+   showFootprints = "0";
+};
+
