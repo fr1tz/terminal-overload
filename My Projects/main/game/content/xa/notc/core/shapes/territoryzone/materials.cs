4267836a35c1e05bb06182db08b666473c77b11a
Michael Goldener <mg@wasted.ch>
2014-05-19 05:31:24 +0200

    Add TerritoryZone materials.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/territoryzone/materials.cs
@@ -0,0 +1,51 @@
+
+singleton Material(xa_notc_core_shapes_territoryzone_terrainmat)
+{
+   diffuseColor[0] = "0.301961 0.658824 0.627451 1";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   parallaxScale[0] = "0.25";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   glow[0] = "0";
+   emissive[0] = "1";
+   materialTag0 = "Miscellaneous";
+   diffuseMap[0] = "content/xa/rotc/p.5.4/textures/rotc/zone.grid.png";
+   animFlags[0] = "0x00000009";
+   scrollDir[0] = "1 1";
+   scrollSpeed[0] = "0.471";
+   rotSpeed[0] = "1.294";
+   rotPivotOffset[0] = "-0.48 -0.52";
+   waveFreq[0] = "0.469";
+   waveAmp[0] = "1";
+};
+
+singleton Material(xa_notc_core_shapes_territoryzone_bordermat)
+{
+   diffuseColor[0] = "0.996078 0.992157 0.996078 1";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   parallaxScale[0] = "0.25";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   glow[0] = "1";
+   emissive[0] = "1";
+   materialTag0 = "Miscellaneous";
+};
+
+singleton Material(xa_notc_core_shapes_territoryzone_othermat)
+{
+   diffuseColor[0] = "0 1 0 1";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   parallaxScale[0] = "0.25";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   glow[0] = "1";
+   emissive[0] = "1";
+   materialTag0 = "Miscellaneous";
+};
+
