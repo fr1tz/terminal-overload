fc3dcee50254844771ebcbd960ee47de5daef724
Michael Goldener <mg@wasted.ch>
2014-05-21 12:29:41 +0200

    Add p2 SMG1 projectile shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/smg1/projectile/p2/materials.cs
@@ -0,0 +1,49 @@
+
+singleton Material(xa_notc_core_shapes_smg1_projectile_p2_decalmat)
+{
+   mapTo = "unmapped_mat";
+   diffuseMap[0] = "content/xa/notc/core/textures/smashdecal2.png";
+   translucent = "1";
+   translucentBlendOp = "AddAlpha";
+   castShadows = "0";
+   showFootprints = "0";
+   glow[0] = "1";
+   emissive[0] = "1";
+   materialTag0 = "Miscellaneous";
+   vertColor[0] = "1";
+};
+
+singleton Material(xa_notc_core_shapes_smg1_projectile_p2_mat1)
+{
+   mapTo = "xa_notc_core_shapes_smg1_projectile_p2_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.996078 1";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   glow[0] = "1";
+   emissive[0] = "1";
+   translucent = "1";
+   materialTag0 = "Miscellaneous";
+   castShadows = "0";
+   showFootprints = "0";
+};
+
+singleton Material(xa_notc_core_shapes_smg1_projectile_p2_mat2)
+{
+   mapTo = "xa_notc_core_shapes_smg1_projectile_p2_mat2";
+   diffuseColor[0] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[0] = "0";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   glow[0] = "1";
+   emissive[0] = "1";
+   diffuseColor[1] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[1] = "0";
+   wireframe[1] = "1";
+   glow[1] = "1";
+   emissive[1] = "1";
+   translucent = "1";
+   materialTag0 = "Miscellaneous";
+   castShadows = "0";
+   showFootprints = "0";
+};
+
