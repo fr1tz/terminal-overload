e43298f54055f4f312e4ff7ebbb57c142483c43c
Michael Goldener <mg@wasted.ch>
2014-05-21 13:22:20 +0200

    Add p1 Etherform shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/etherform/p1/materials.cs
@@ -0,0 +1,32 @@
+
+singleton Material(xa_notc_core_shapes_etherform_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_etherform_p1_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.996078 1";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   glow[0] = "1";
+   wireframe[0] = "0";
+   emissive[0] = "1";
+   translucent = "1";
+   materialTag0 = "Miscellaneous";
+   castShadows = "0";
+   showFootprints = "0";
+};
+
+singleton Material(xa_notc_core_shapes_etherform_p1_mat2)
+{
+   mapTo = "xa_notc_core_shapes_etherform_p1_mat2";
+   diffuseColor[0] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[0] = "0";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   glow[0] = "1";
+   wireframe[0] = "0";
+   emissive[0] = "1";
+   translucent = "1";
+   materialTag0 = "Miscellaneous";
+   castShadows = "0";
+   showFootprints = "0";
+};
+
---
+++ My Projects/main/game/content/xa/notc/core/shapes/etherform/p1/shape.cs
@@ -0,0 +1,6 @@
+
+singleton TSShapeConstructor(xa_notc_core_shapes_etherform_p1_dts)
+{
+   baseShape = "./shape.dae";
+};
+
