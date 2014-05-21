1f63f785f6a28028237da00aacac1e067b7743ac
Michael Goldener <mg@wasted.ch>
2014-05-21 06:31:10 +0200

    Start work on ETH1 map.

---
+++ My Projects/main/game/content/xa/notc/mis/eth1/materials.cs
@@ -0,0 +1,45 @@
+
+singleton Material(xa_notc_mis_eth1_termat1fx)
+{
+   mapTo = "xa_notc_mis_eth1_termat1";
+   footstepSoundId = "2";
+   terrainMaterials = "1";
+   ShowDust = "true";
+   showFootprints = "1";
+   materialTag0 = "Terrain";
+   effectColor[0] = "0.00784314 0.94902 0.996078 1";
+   effectColor[1] = "0.996078 0.00784314 0.901961 1";
+   glow[0] = "0";
+   emissive[0] = "0";
+   detailScale[0] = "64 64";
+   scrollDir[0] = "0 -0.03";
+   scrollSpeed[0] = "1.647";
+   castShadows = "0";
+   translucentBlendOp = "None";
+   parallaxScale[0] = "1";
+   useAnisotropic[0] = "1";
+   diffuseColor[0] = "0.0235294 1 0 1";
+   diffuseMap[1] = "content/xa/notc/mis/dm1/textures/grid1.png";
+   glow[1] = "1";
+   emissive[1] = "1";
+   detailScale[2] = "64 64";
+   detailScale[1] = "64 64";
+   diffuseColor[2] = "0.00392157 0.00392157 0.00392157 1";
+   diffuseColor[1] = "0.992157 0.992157 0.992157 1";
+   impactSoundId = "2";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "content/xa/notc/mis/eth1/textures/xa_notc_mis_eth1_termat1";
+   detailMap = "content/xa/rotc/p.5.4/textures/rotc/zone.grid";
+   internalName = "xa_notc_mis_eth1_termat1";
+   diffuseSize = "16";
+   detailSize = "4";
+   detailDistance = "9999";
+   macroMap = "content/xa/rotc/p.5.4/textures/rotc/zone.grid";
+   macroStrength = "0";
+   detailStrength = "0.2";
+   macroSize = "8";
+   macroDistance = "9999";
+};
---
+++ My Projects/main/game/content/xa/notc/mis/eth1/shapes/cube1/materials.cs
@@ -0,0 +1,42 @@
+
+singleton Material(xa_notc_mis_eth1_shapes_cube1_mat1)
+{
+   mapTo = "xa_notc_mis_eth1_shapes_cube1_mat1";
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
+   diffuseColorPaletteSlot[0] = 0;
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   specularStrength[0] = "1.96078";
+   pixelSpecular[0] = "0";
+   diffuseColor[1] = "0.996078 0.996078 0.992157 1";
+   diffuseColorPaletteSlot[1] = 1;
+   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.1.png";
+   emissive[1] = "1";
+   diffuseColor[2] = "0.996078 0.996078 0.992157 1";
+   diffuseColorPaletteSlot[2] = 1;
+   diffuseMap[2] = "content/xa/notc/core/textures/grid.128.1.png";
+   emissive[2] = "1";
+   wireframe[2] = "1";
+   footstepSoundId = "2";
+   impactSoundId = "-1";
+   materialTag0 = "Miscellaneous";
+};
+
+singleton Material(shape_xa_notc_mis_eth1_shapes_cube1_mat1)
+{
+   mapTo = "xa_notc_mis_eth1_shapes_cube1_mat1";
+   diffuseColor[0] = "0.431373 0.431373 0.431373 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   pixelSpecular[0] = "1";
+};
+
+singleton Material(shape_xa_notc_mis_eth1_shapes_cube1_mat2)
+{
+   mapTo = "xa_notc_mis_eth1_shapes_cube1_mat2";
+   diffuseColor[0] = "0.580392 0.00784314 0.996078 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   glow[0] = "1";
+   emissive[0] = "1";
+};
