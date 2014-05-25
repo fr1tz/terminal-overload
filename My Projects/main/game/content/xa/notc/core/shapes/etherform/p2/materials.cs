4244e1f53f06ae598f82814e253a2ec63fad9b77
Michael Goldener <mg@wasted.ch>
2014-05-25 11:02:54 +0200

    Add p2 Etherform shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/etherform/p2/materials.cs
@@ -0,0 +1,17 @@
+
+singleton Material(xa_notc_core_shapes_etherform_p2_mat1)
+{
+   mapTo = "xa_notc_core_shapes_etherform_p2_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[0] = "0";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "0";
+   translucentBlendOp = "Add";
+   diffuseMap[0] = "content/xa/notc/core/shapes/etherform/p2/core.png";
+   glow[0] = "1";
+   emissive[0] = "1";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+};
---
+++ My Projects/main/game/content/xa/notc/core/shapes/etherform/p2/shape.cs
@@ -0,0 +1,7 @@
+
+singleton TSShapeConstructor(xa_notc_core_shapes_etherform_p2_dts)
+{
+   baseShape = "./shape.dae";
+   loadLights = "0";
+};
+
