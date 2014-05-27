1242fb2b9355fb3f05d765efa67ff30c41cce487
Michael Goldener <mg@wasted.ch>
2014-05-27 20:31:53 +0200

    Add p2 MG1 projectile shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/mg1/projectile/p2/materials.cs
@@ -0,0 +1,41 @@
+
+singleton Material(xa_notc_core_shapes_mg1_projectile_p2_mat1)
+{
+   mapTo = "xa_notc_core_shapes_mg1_projectile_p2_mat1";
+   diffuseColor[0] = "0.992157 0.996078 0.996078 1";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   glow[0] = "1";
+   emissive[0] = "1";
+   translucent = "1";
+   materialTag0 = "Miscellaneous";
+   castShadows = "0";
+   showFootprints = "0";
+   glow[1] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
+   diffuseColor[1] = "0.996078 0.992157 0.996078 1";
+   diffuseColorPaletteSlot[0] = "0";
+};
+
+singleton Material(xa_notc_core_shapes_mg1_projectile_p2_mat2)
+{
+   mapTo = "xa_notc_core_shapes_mg1_projectile_p2_mat2";
+   diffuseColor[0] = "0.0901961 0.0901961 0.0901961 1";
+   diffuseColorPaletteSlot[0] = "0";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   glow[0] = "1";
+   emissive[0] = "1";
+   diffuseColor[1] = "0.992157 0.992157 0.996078 0";
+   diffuseColorPaletteSlot[1] = "-1";
+   wireframe[1] = "0";
+   glow[1] = "0";
+   emissive[1] = "0";
+   translucent = "1";
+   materialTag0 = "Miscellaneous";
+   castShadows = "0";
+   showFootprints = "0";
+   glowOnly[0] = "1";
+};
+
---
+++ My Projects/main/game/content/xa/notc/core/shapes/mg1/projectile/p2/shape.cs
@@ -0,0 +1,7 @@
+
+singleton TSShapeConstructor(xa_notc_core_shapes_mg1_projectile_p2_dts)
+{
+   baseShape = "./shape.dae";
+   loadLights = "0";
+};
+
