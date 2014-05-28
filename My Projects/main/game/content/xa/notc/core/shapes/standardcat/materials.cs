31d6a67fa65c6b29c73ed5ae53047bcbc7a05b9e
Michael Goldener <mg@wasted.ch>
2014-05-28 23:14:50 +0200

    Tweak standard CAT materials.

--- My Projects/main/game/content/xa/notc/core/shapes/standardcat/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/standardcat/materials.cs
@@ -63,24 +63,18 @@ singleton Material(xa_notc_core_shapes_standardcat_trailsmat)
 singleton Material(xa_notc_core_shapes_standardcat_mat2)
 {
    mapTo = "armor_red0024";
-   emissive[0] = "0";
+   emissive[0] = "1";
    glow[0] = "0";
    diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    diffuseColorPaletteSlot[0] = "0";
    diffuseMap[0] = "content/xa/notc/core/shapes/standardcat/armor.png";
-   normalMap[0] = "content/xa/notc/core/textures/grid128.12.h2.n2.png";
-   pixelSpecular[0] = "1";
-   emissive[1] = "1";
-   glow[1] = "1";
-   diffuseColor[1] = "0.992157 0.992157 0.992157 1";
-   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.4.png";
+   pixelSpecular[0] = "0";
+   diffuseColor[1] = "0.996078 0.996078 0.996078 1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
    diffuseColorPaletteSlot[1] = "0";
-   diffuseColor[2] = "0.996078 0.996078 0.996078 1";
-   diffuseColorPaletteSlot[2] = "0";
-   diffuseMap[2] = "content/xa/notc/core/textures/grid128.12.png";
-   glow[2] = "1";
-   emissive[2] = "1";
-   wireframe[2] = "1";
+   glow[1] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
    materialTag0 = "Miscellaneous";
    doubleSided = "0";
    translucent = "0";
@@ -103,5 +97,12 @@ singleton Material(xa_notc_core_shapes_standardcat_mat1)
    translucentZWrite = "0";
    translucentBlendOp = "None";
    emissive[0] = "1";
+   glow[0] = "0";
+   diffuseColor[1] = "0.988235 0.996078 0.996078 1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
+   diffuseColorPaletteSlot[1] = "1";
+   glow[1] = "0";
+   wireframe[1] = "1";
+   emissive[1] = "1";
 };
 
