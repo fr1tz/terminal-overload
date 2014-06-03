6d8f8cf33a03f2dcad4d7310889b4932916dd029
Michael Goldener <mg@wasted.ch>
2014-06-03 17:32:04 +0200

    Tweak TerritoryZone materials.

--- My Projects/main/game/content/xa/notc/core/shapes/territoryzone/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/territoryzone/materials.cs
@@ -1,7 +1,7 @@
 
 singleton Material(xa_notc_core_shapes_territoryzone_terrainmat)
 {
-   diffuseColor[0] = "0.435294 0.435294 0.435294 1";
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    diffuseColorPaletteSlot[0] = "0";
    doubleSided = "0";
    translucentBlendOp = "AddAlpha";
@@ -12,20 +12,26 @@ singleton Material(xa_notc_core_shapes_territoryzone_terrainmat)
    glow[0] = "0";
    emissive[0] = "1";
    materialTag0 = "Miscellaneous";
-   diffuseMap[0] = "content/xa/rotc/p.5.4/textures/rotc/zone.png";
+   diffuseMap[0] = "content/xa/rotc/p.5.4/textures/rotc/armor.white.png";
    animFlags[0] = "0x00000009";
    scrollDir[0] = "1 1";
-   scrollSpeed[0] = "0.471";
+   scrollSpeed[0] = "0.235";
    rotSpeed[0] = "1.294";
    rotPivotOffset[0] = "-0.48 -0.52";
    waveFreq[0] = "0.469";
    waveAmp[0] = "1";
    mapTo = "zone.grid.png";
+   wireframe[0] = "1";
+   diffuseColorPaletteSlot[1] = "-1";
+   glow[1] = "0";
+   emissive[1] = "0";
+   wireframe[1] = "0";
+   diffuseColor[1] = "0.996078 0.988235 0.988235 0";
 };
 
 singleton Material(xa_notc_core_shapes_territoryzone_bordermat)
 {
-   diffuseColor[0] = "0.996078 0.992157 0.996078 1";
+   diffuseColor[0] = "0.992157 0.988235 0.992157 1";
    diffuseColorPaletteSlot[0] = "0";
    doubleSided = "0";
    translucentBlendOp = "AddAlpha";
@@ -40,7 +46,7 @@ singleton Material(xa_notc_core_shapes_territoryzone_bordermat)
 
 singleton Material(xa_notc_core_shapes_territoryzone_othermat)
 {
-   diffuseColor[0] = "0.239216 0.239216 0.239216 1";
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    diffuseColorPaletteSlot[0] = "0";
    doubleSided = "0";
    translucentBlendOp = "AddAlpha";
