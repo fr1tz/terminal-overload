a0a341ee17692c0beca72103de6d1ef3300c77ea
Michael Goldener <mg@wasted.ch>
2014-06-06 02:39:03 +0200

    Tweak p2 MG1 projectile visuals.

--- My Projects/main/game/content/xa/notc/core/shapes/mg1/projectile/p2/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/mg1/projectile/p2/materials.cs
@@ -14,14 +14,17 @@ singleton Material(xa_notc_core_shapes_mg1_projectile_p2_mat1)
    glow[1] = "1";
    emissive[1] = "1";
    wireframe[1] = "1";
-   diffuseColor[1] = "0.996078 0.992157 0.996078 1";
-   diffuseColorPaletteSlot[0] = "0";
+   diffuseColor[1] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[0] = "-1";
+   diffuseColorPaletteSlot[1] = "0";
+   glowOnly[1] = "0";
+   wireframe[0] = "0";
 };
 
 singleton Material(xa_notc_core_shapes_mg1_projectile_p2_mat2)
 {
    mapTo = "xa_notc_core_shapes_mg1_projectile_p2_mat2";
-   diffuseColor[0] = "0.0901961 0.0901961 0.0901961 1";
+   diffuseColor[0] = "0.564706 0.564706 0.564706 1";
    diffuseColorPaletteSlot[0] = "0";
    doubleSided = "0";
    translucentBlendOp = "AddAlpha";
