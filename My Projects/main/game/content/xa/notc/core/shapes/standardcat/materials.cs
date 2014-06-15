6f829ead9525403bc9b5a7497e8c1f1a28a4019d
Michael Goldener <mg@wasted.ch>
2014-06-15 03:36:17 +0200

    Add standard CAT stealth materials.

--- My Projects/main/game/content/xa/notc/core/shapes/standardcat/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/standardcat/materials.cs
@@ -62,6 +62,28 @@ singleton Material(xa_notc_core_shapes_standardcat_trailsmat)
    glowOnly[0] = "0";
 };
 
+singleton Material(xa_notc_core_shapes_standardcat_mat1)
+{
+   mapTo = "base";
+   diffuseColor[0] = "1 1 1 1";
+   diffuseColorPaletteSlot[0] = "1";
+   diffuseMap[0] = "content/xa/notc/core/shapes/standardcat/armor.png";
+   showFootprints = "0";
+   materialTag0 = "Miscellaneous";
+   doubleSided = "0";
+   translucent = "0";
+   translucentZWrite = "0";
+   translucentBlendOp = "None";
+   emissive[0] = "1";
+   glow[0] = "0";
+   diffuseColor[1] = "0.988235 0.996078 0.996078 1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
+   diffuseColorPaletteSlot[1] = "1";
+   glow[1] = "0";
+   wireframe[1] = "1";
+   emissive[1] = "1";
+};
+
 singleton Material(xa_notc_core_shapes_standardcat_mat2)
 {
    mapTo = "armor_red0024";
@@ -86,25 +108,65 @@ singleton Material(xa_notc_core_shapes_standardcat_mat2)
    showFootprints = "0";
 };
 
-singleton Material(xa_notc_core_shapes_standardcat_mat1)
+singleton Material(xa_notc_core_shapes_standardcat_stealthmat1)
 {
-   mapTo = "base";
+   mapTo = "xa_notc_core_shapes_standardcat_stealthmat1";
+   emissive[0] = "1";
+   glow[0] = "1";
    diffuseColor[0] = "1 1 1 1";
-   diffuseColorPaletteSlot[0] = "1";
+   diffuseColorPaletteSlot[0] = "-1";
    diffuseMap[0] = "content/xa/notc/core/shapes/standardcat/armor.png";
-   showFootprints = "0";
+   pixelSpecular[0] = "0";
+   diffuseColor[1] = "0.996078 0.996078 0.996078 1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
+   diffuseColorPaletteSlot[1] = "-1";
+   glow[1] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
    materialTag0 = "Miscellaneous";
    doubleSided = "0";
-   translucent = "0";
+   translucent = "1";
    translucentZWrite = "0";
-   translucentBlendOp = "None";
-   emissive[0] = "1";
-   glow[0] = "0";
-   diffuseColor[1] = "0.988235 0.996078 0.996078 1";
-   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
-   diffuseColorPaletteSlot[1] = "1";
-   glow[1] = "0";
-   wireframe[1] = "1";
-   emissive[1] = "1";
+   translucentBlendOp = "AddAlpha";
+   alphaRef = "1";
+   showFootprints = "0";
+   wireframe[0] = "0";
+   glowOnly[0] = "1";
+   animFlags[0] = "0x00000001";
+   scrollDir[0] = "1 1";
+   scrollSpeed[0] = "1";
+   animFlags[1] = "0x00000001";
+   scrollDir[1] = "1 1";
+   scrollSpeed[1] = "2";
 };
+
+singleton Material(xa_notc_core_shapes_standardcat_stealthmat2)
+{
+   mapTo = "xa_notc_core_shapes_standardcat_stealthmat2";
+   diffuseColor[0] = "0.0980392 0.0980392 0.0980392 1";
+   diffuseColor[1] = "0.992157 0.992157 0.992157 1";
+   diffuseColorPaletteSlot[0] = "0";
+   diffuseColorPaletteSlot[1] = "0";
+   diffuseMap[0] = "content/xa/rotc/p.5.4/textures/rotc/armor.white.png";
+   diffuseMap[1] = "content/xa/notc/core/shapes/standardcat/stealth1.png";
+   glow[0] = "1";
+   glow[1] = "1";
+   glowOnly[0] = "1";
+   emissive[0] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
+   translucent = "1";
+   translucentBlendOp = "AddAlpha";
+   alphaRef = "0";
+   showFootprints = "0";
+   materialTag0 = "Miscellaneous";
+   animFlags[1] = "0x00000001";
+   scrollDir[1] = "1 1";
+   scrollSpeed[1] = "2";
+   animFlags[0] = "0x00000001";
+   scrollDir[0] = "1 1";
+   scrollSpeed[0] = "0.5";
+};
+
+
 
