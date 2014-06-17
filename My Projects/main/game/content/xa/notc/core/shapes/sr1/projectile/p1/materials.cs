97222130c2b17ded1d9fed72a250b494b299031c
Michael Goldener <mg@wasted.ch>
2014-06-17 02:41:03 +0200

    Add p1 SR1 decal texture.

--- My Projects/main/game/content/xa/notc/core/shapes/sr1/projectile/p1/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/sr1/projectile/p1/materials.cs
@@ -1,3 +1,20 @@
+
+singleton Material(xa_notc_core_shapes_sr1_projectile_p1_decalmat)
+{
+   diffuseMap[0] = "content/xa/notc/core/shapes/sr1/projectile/p1/decal.png";
+   translucent = "1";
+   translucentBlendOp = "AddAlpha";
+   castShadows = "0";
+   showFootprints = "0";
+   glow[0] = "1";
+   emissive[0] = "1";
+   diffuseColor[0] = "0.996078 0.996078 0.992157 1";
+   translucentZWrite = "1";
+   vertColor[0] = "1";
+   materialTag0 = "Miscellaneous";
+   mapTo = "corona.png";
+};
+
 singleton Material(xa_notc_core_shapes_sr1_projectile_p1_lasertrail0mat)
 {
    diffuseColor[0] = "0.996078 0.996078 0.996078 1";
