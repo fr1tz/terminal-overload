df8112c6f554e8310e8d412de26c2d8a6163c10b
Michael Goldener <mg@wasted.ch>
2014-06-19 22:47:59 +0200

    Add p2 SMG1 image shapes.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/smg1/image/p2/materials.cs
@@ -0,0 +1,45 @@
+
+singleton Material(xa_notc_core_shapes_smg1_image_p2_mat1)
+{
+   mapTo = "xa_notc_core_shapes_smg1_image_p2_mat1";
+   diffuseMap[0] = "content/xa/notc/core/textures/grid.128.4.h1.png";
+   diffuseColor[0] = "0.992157 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[0] = "1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   glow[0] = "0";
+   emissive[0] = "1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
+   diffuseColor[1] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[1] = "1";
+   wireframe[1] = "1";
+   emissive[1] = "1";
+   glow[1] = "0";
+   doubleSided = "0";
+   translucentBlendOp = "LerpAlpha";
+   castShadows = "0";
+   translucent = "0";
+   showFootprints = "0";
+   materialTag0 = "Miscellaneous";
+};
+
+singleton Material(xa_notc_core_shapes_smg1_image_p2_mat2)
+{
+   mapTo = "xa_notc_core_shapes_smg1_image_p2_mat2";
+   diffuseColor[0] = "0.996078 0.992157 0.996078 1";
+   diffuseColor[1] = "0.00392157 0.00392157 0.00392157 1";
+   diffuseMap[0] = "content/xa/notc/core/shapes/smg1/image/p1/muzzleflash.png";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   glow[0] = "0";
+   emissive[0] = "1";
+   doubleSided = "1";
+   castShadows = "0";
+   translucent = "1";
+   translucentBlendOp = "AddAlpha";
+   showFootprints = "0";
+   translucentZWrite = "0";
+   materialTag0 = "Miscellaneous";
+};
+
+
