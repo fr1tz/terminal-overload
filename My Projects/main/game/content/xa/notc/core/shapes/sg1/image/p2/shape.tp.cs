6672ce512f880aa4d60018d59e567e8e8a00c38a
Michael Goldener <mg@wasted.ch>
2014-06-20 20:33:58 +0200

    Add p2 third-person image shape for SG1.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/sg1/image/p2/shape.tp.cs
@@ -0,0 +1,19 @@
+
+singleton TSShapeConstructor(xa_notc_core_shapes_sg1_image_p2_tp_dts)
+{
+   baseShape = "./shape.tp.dae";
+   lodType = "TrailingNumber";
+   loadLights = "0";
+};
+
+function xa_notc_core_shapes_sg1_image_p2_tp_dts::onLoad(%this)
+{
+   %this.renameSequence("ambient", "timeline");
+   %this.setSequenceCyclic("timeline", "0");
+   %this.addSequence("timeline", "idle", "0", "10", "1", "0");
+   %this.addSequence("timeline", "reload", "50", "60", "1", "0");
+   %this.addSequence("timeline", "empty", "40", "45", "1", "0");
+   %this.setSequenceCyclic("empty", "1");
+   %this.setSequenceCyclic("idle", "1");
+   %this.addSequence("timeline", "fire", "30", "40", "1", "0");
+}
