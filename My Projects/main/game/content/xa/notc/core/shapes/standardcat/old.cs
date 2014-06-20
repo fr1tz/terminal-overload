2099c368b9816449e0125e8b39d7df8cf96337f8
Michael Goldener <mg@wasted.ch>
2014-06-20 20:33:29 +0200

    Add "aimarmcannon" sequence to standard CAT.

--- My Projects/main/game/content/xa/notc/core/shapes/standardcat/old.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/standardcat/old.cs
@@ -51,4 +51,5 @@ function xa_notc_core_shaapes_standardcat_old_dts::onLoad(%this)
    %this.renameNode("pelvis", "Mount3");
    %this.renameNode("head", "Mount4");
    %this.addSequence("./oldanims/a/holdgun_onehand.dsq", "holdgun_onehand", "0", "15", "1", "0");
+   %this.addSequence("./oldanims/ub/aimarmcannon.dsq", "aimarmcannon", "0", "15", "1", "0");
 }
