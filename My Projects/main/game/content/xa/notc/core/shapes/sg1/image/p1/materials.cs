626e3b865a935d5572af67a4ee60d22ed606baf7
Michael Goldener <mg@wasted.ch>
2014-06-20 06:45:48 +0200

    Work on p1 SG1 image shapes.

--- My Projects/main/game/content/xa/notc/core/shapes/sg1/image/p1/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/sg1/image/p1/materials.cs
@@ -56,3 +56,12 @@ singleton Material(xa_notc_core_shapes_sg1_image_p1_mat2)
 };
 
 
+
+singleton Material(shape_tp_Material_001)
+{
+   mapTo = "Material_001";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
