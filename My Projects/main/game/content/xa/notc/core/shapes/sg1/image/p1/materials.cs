dce319c44553eb2fb1180c0a88057c806305f7ad
Michael Goldener <mg@wasted.ch>
2014-06-20 02:54:18 +0200

    Add p1 first-person image shape for SG1.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/sg1/image/p1/materials.cs
@@ -0,0 +1,58 @@
+
+singleton Material(xa_notc_core_shapes_sg1_image_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_sg1_image_p1_mat1";
+   diffuseMap[0] = "content/xa/notc/core/textures/grid.128.4.h1.png";
+   diffuseColor[0] = "0.992157 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[0] = "1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   glow[0] = "0";
+   emissive[0] = "1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
+   diffuseColor[1] = "0.996078 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[1] = "1";
+   wireframe[1] = "1";
+   emissive[1] = "1";
+   glow[1] = "0";
+   doubleSided = "0";
+   translucentBlendOp = "LerpAlpha";
+   castShadows = "0";
+   translucent = "0";
+   showFootprints = "0";
+   materialTag0 = "Miscellaneous";
+};
+
+singleton Material(xa_notc_core_shapes_sg1_image_p1_mat2)
+{
+   mapTo = "xa_notc_core_shapes_sg1_image_p1_mat2";
+   diffuseColor[0] = "0.996078 0.996078 0.996078 1";
+   diffuseColor[1] = "0.988235 0.988235 0.988235 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   glow[0] = "1";
+   emissive[0] = "1";
+   doubleSided = "0";
+   castShadows = "0";
+   translucent = "0";
+   translucentBlendOp = "AddAlpha";
+   showFootprints = "0";
+   translucentZWrite = "0";
+   materialTag0 = "Miscellaneous";
+   diffuseColorPaletteSlot[0] = "0";
+   wireframe[0] = "0";
+   effectColor[0] = "0 0 0 0";
+   glow[1] = "1";
+   diffuseColorPaletteSlot[1] = "-1";
+   emissive[1] = "1";
+   wireframe[1] = "0";
+   animFlags[0] = "0x00000000";
+   scrollDir[0] = "0 0";
+   scrollSpeed[0] = "0";
+   diffuseMap[1] = "content/xa/notc/core/shapes/standardcat/grid0.png";
+   animFlags[1] = "0x00000001";
+   scrollDir[1] = "1 1";
+   scrollSpeed[1] = "10";
+};
+
+
---
+++ My Projects/main/game/content/xa/notc/core/shapes/sg1/image/p1/shape.fp.cs
@@ -0,0 +1,17 @@
+
+singleton TSShapeConstructor(xa_notc_core_shapes_sg1_image_p1_fp_dts)
+{
+   baseShape = "./shape.fp.dae";
+   loadLights = "0";
+};
+
+function xa_notc_core_shapes_sg1_image_p1_fp_dts::onLoad(%this)
+{
+   %this.renameSequence("ambient", "timeline");
+   %this.addSequence("timeline", "fire", "100", "110", "1", "0");
+   %this.setSequenceCyclic("fire", "0");
+   %this.addSequence("timeline", "idle", "50", "80", "1", "0");
+   %this.addSequence("timeline", "reload", "130", "160", "1", "0");
+   %this.setSequenceCyclic("reload", "0");
+   %this.addSequence("timeline", "empty", "125", "126", "1", "0");
+}
