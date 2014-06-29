a43081a630070014c50e4d96f28a6facbd97b93a
Michael Goldener <mg@wasted.ch>
2014-06-29 15:38:20 +0200

    Change default cursor.

--- My Projects/main/game/core/scripts/gui/cursors.cs
+++ My Projects/main/game/core/scripts/gui/cursors.cs
@@ -1,21 +1,10 @@
 // Copyright information can be found in the file named COPYING
 // located in the root directory of this distribution.
 
-if($platform $= "macos")
+new GuiCursor(DefaultCursor)
 {
-   new GuiCursor(DefaultCursor)
-   {
-      hotSpot = "4 4";
-      renderOffset = "0 0";
-      bitmapName = "~/art/gui/images/macCursor";
-   };
-} 
-else 
-{
-   new GuiCursor(DefaultCursor)
-   {
-      hotSpot = "1 1";
-      renderOffset = "0 0";
-      bitmapName = "~/art/gui/images/defaultCursor";
-   };
-}
+   hotSpot = "4 4";
+   renderOffset = "0 0";
+   bitmapName = "~/art/gui/images/notcCursor";
+};
+
