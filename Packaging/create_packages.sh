f12665157b6ca95bcd1171d956e39fba6a26debb
Michael Goldener <mg@wasted.ch>
2014-07-06 16:51:37 +0200

    Work on packaging scripts.

--- Packaging/create_packages.sh
+++ Packaging/create_packages.sh
@@ -15,30 +15,30 @@ create_packages()
 	cd packages/ || abort
 	
 	SOURCE_DIST_NAME="terminal-overload-$GAME_VERSION-source"
-	LINUX_DIST_NAME="terminal-overload-$GAME_VERSION-linux"
+	LINUX32_DIST_NAME="terminal-overload-$GAME_VERSION-linux32"
 	WIN32_DIST_NAME="terminal-overload-$GAME_VERSION-win32"
 
 	test -e $SOURCE_DIST_NAME && rm -r $SOURCE_DIST_NAME
-	test -e $LINUX_DIST_NAME && rm -r $LINUX_DIST_NAME
+	test -e $LINUX32_DIST_NAME && rm -r $LINUX32_DIST_NAME
 	test -e $WIN32_DIST_NAME && rm -r $WIN32_DIST_NAME
 
 	mv ../dists/source $SOURCE_DIST_NAME || abort
-	mv ../dists/linux $LINUX_DIST_NAME || abort
+	mv ../dists/linux32 $LINUX32_DIST_NAME || abort
 	mv ../dists/win32 $WIN32_DIST_NAME || abort
 
 	tar -czf $SOURCE_DIST_NAME.tar.gz $SOURCE_DIST_NAME
-	tar -czf $LINUX_DIST_NAME.tar.gz $LINUX_DIST_NAME
+	tar -czf $LINUX32_DIST_NAME.tar.gz $LINUX32_DIST_NAME
 	zip -r $WIN32_DIST_NAME.zip $WIN32_DIST_NAME
 
 	echo 'Done creating packages!'
 )
 
-for CMD in cat tar zip
+for CMD in chrpath cat tar zip
 do
 	which $CMD || abort "Required command not found: $CMD"
 done
 
-for DIR in dists/source dists/linux dists/win32 packages
+for DIR in dists/source dists/linux32 dists/win32 packages
 do
 	test -d $DIR || abort "$DIR not found."
 done
--- Packaging/create_platform_dists.sh
+++ Packaging/create_platform_dists.sh
@@ -12,11 +12,11 @@ create_platform_dists()
 	echo "Creating platform distributions..."
 	cd dists/ || abort
 
-	test -d linux && rm -r linux
+	test -d linux32 && rm -r linux32
 	test -d win32 && rm -r win32
 
-	cp -r 'source/My Projects/main/game' linux || abort
-	FIND_ARGS="linux"
+	cp -r 'source/My Projects/main/game' linux32 || abort
+	FIND_ARGS="linux32"
 	for EXT in dmp psd src.png max xcf blend dae
 	do
 		FIND_ARGS="$FIND_ARGS -name *.$EXT -execdir rm {} ; -o"
@@ -24,12 +24,14 @@ create_platform_dists()
 	FIND_ARGS="$FIND_ARGS -false"
 	#echo "find $FIND_ARGS"
 	find $FIND_ARGS
-	cp -r linux win32 || abort
+	cp -r linux32 win32 || abort
 
-	rm linux/*.dll || abort
-	rm win32/lib* || abort
-	cp ../bins/linux/* linux/ || abort
+	rm linux32/*.dll || abort
+	rm -r win32/lib || abort
+	cp ../bins/linux32/* linux32/ || abort
 	cp ../bins/win32/* win32/ || abort
+
+	chrpath -r '$ORIGIN/lib' dists/linux32/overload
 )
 
 create_platform_dists
