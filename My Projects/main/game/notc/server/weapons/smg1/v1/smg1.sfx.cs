898a499d3f66b4641c01bd235a0ede73b96845fc
Michael Goldener <mg@wasted.ch>
2014-07-26 04:37:08 -0700

    Get rid of unused datablocks.

--- My Projects/main/game/notc/server/weapons/smg1/v1/smg1.sfx.cs
+++ My Projects/main/game/notc/server/weapons/smg1/v1/smg1.sfx.cs
@@ -1,12 +1,6 @@
 // Copyright information can be found in the file named COPYING
 // located in the root directory of this distribution.
 
-datablock SFXPlayList(WpnSMG1FireSoundList)
-{
-   description = AudioClose3D;
-   track[0] = WpnSMG1FireSound;
-};
-
 datablock SFXProfile(WpnSMG1FireSound)
 {
    filename = "content/xa/cat5/p.1/sounds/cat5/charge4fire";
