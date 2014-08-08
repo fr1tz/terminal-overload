3a12b0268eb83378ef45d70dca37e7ac7b3b2176
Michael Goldener <mg@wasted.ch>
2014-08-08 09:44:18 -0700

    Tweak territory zone terrain material.

--- My Projects/main/game/content/xa/notc/core/shapes/territoryzone/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/territoryzone/materials.cs
@@ -12,20 +12,20 @@ singleton Material(xa_notc_core_shapes_territoryzone_terrainmat)
    glow[0] = "0";
    emissive[0] = "1";
    materialTag0 = "Miscellaneous";
-   animFlags[0] = "0x00000008";
-   scrollDir[0] = "1 1";
-   scrollSpeed[0] = "0";
+   animFlags[0] = "0x00000000";
+   scrollDir[0] = "0.3 1";
+   scrollSpeed[0] = "0.5";
    rotSpeed[0] = "1.294";
    rotPivotOffset[0] = "-0.48 -0.52";
-   waveFreq[0] = "0.469";
+   waveFreq[0] = "0.938";
    waveAmp[0] = "1";
    mapTo = "zone.grid.png";
    wireframe[0] = "0";
-   diffuseColorPaletteSlot[1] = "-1";
+   diffuseColorPaletteSlot[1] = "0";
    glow[1] = "0";
    emissive[1] = "0";
    wireframe[1] = "0";
-   diffuseColor[1] = "0.996078 0.988235 0.988235 0";
+   diffuseColor[1] = "0.996078 0.996078 0.996078 0";
    diffuseMap[0] = "content/xa/notc/core/shapes/territoryzone/terrain.png";
 };
 
