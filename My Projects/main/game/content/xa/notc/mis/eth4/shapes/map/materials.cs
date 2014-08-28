36d3dc4a9a5cc4b2a55cf68175fbb4488d9396f7
Michael Goldener <mg@wasted.ch>
2014-08-28 06:10:21 -0700

    Start work on ETH4 map.

---
+++ My Projects/main/game/content/xa/notc/mis/eth4/shapes/map/materials.cs
@@ -0,0 +1,37 @@
+
+singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat1)
+{
+   mapTo = "xa_notc_mis_eth4_shapes_map_mat1";
+   diffuseColor[0] = "0.176471 0.176471 0.176471 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+   diffuseColor[1] = "0.992157 0.992157 0.992157 1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.4.png";
+   emissive[1] = "1";
+   detailScale[0] = "16 16";
+   detailNormalMapStrength[0] = "1";
+   diffuseMap[0] = "content/xa/notc/core/textures/white.128.png";
+   pixelSpecular[0] = "0";
+   materialTag0 = "Miscellaneous";
+   detailMap[0] = "content/xa/notc/core/textures/grid.128.4.png";
+};
+
+singleton Material(struct1_xa_notc_mis_eth4_shapes_map_gridmat)
+{
+   mapTo = "xa_notc_mis_eth4_shapes_map_gridmat";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
+
+singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat2)
+{
+   mapTo = "xa_notc_mis_eth4_shapes_map_mat2";
+   diffuseColor[0] = "0.443137 0.443137 0.443137 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+   materialTag0 = "Miscellaneous";
+};
---
+++ My Projects/main/game/content/xa/notc/mis/eth4/shapes/map/struct1.cs
@@ -0,0 +1,7 @@
+
+singleton TSShapeConstructor(Struct1Dae)
+{
+   baseShape = "./struct1.dae";
+   lodType = "TrailingNumber";
+   loadLights = "0";
+};
