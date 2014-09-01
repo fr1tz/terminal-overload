1dd950ad80d3cd1369c5bf5d2551134d484bfddd
Michael Goldener <mg@wasted.ch>
2014-09-01 03:12:41 -0700

    Move loading of all forms/weapons/items scripts into executeBaseServerScripts().

--- My Projects/main/game/notc/server/dm/exec.cs
+++ My Projects/main/game/notc/server/dm/exec.cs
@@ -4,32 +4,6 @@
 function executeDeathmatchServerScripts()
 {
    exec("notc/server/base/exec.cs");
-   
-   exec("notc/server/forms/standardcat/exec.cs");
-
-   exec("notc/server/items/damper/exec.cs");
-   exec("notc/server/items/vamp/exec.cs");
-   exec("notc/server/items/impshield/exec.cs");
-   exec("notc/server/items/ballast/exec.cs");
-   exec("notc/server/items/stealth/exec.cs");
-   exec("notc/server/items/etherboard/exec.cs");
-   exec("notc/server/items/launcher/exec.cs");
-   exec("notc/server/items/bounce/exec.cs");
-
-   exec("notc/server/items/g1launcher/v1/exec.cs");
-
-   exec("notc/server/weapons/razordisc/v1/exec.cs");
-   exec("notc/server/weapons/explosivedisc/v1/exec.cs");
-   exec("notc/server/weapons/repeldisc/v1/exec.cs");
-   exec("notc/server/weapons/interceptordisc/v1/exec.cs");
-   
-   exec("notc/server/weapons/smg1/v1/exec.cs");
-   exec("notc/server/weapons/mgl1/v1/exec.cs");
-   exec("notc/server/weapons/sg1/v1/exec.cs");
-   exec("notc/server/weapons/sg2/v1/exec.cs");
-   exec("notc/server/weapons/sr1/v1/exec.cs");
-   exec("notc/server/weapons/mg1/v1/exec.cs");
-   exec("notc/server/weapons/ml1/v1/exec.cs");
 
    exec("./aiPlayer.cs");
    exec("./dm.cs");
--- My Projects/main/game/notc/server/eth/exec.cs
+++ My Projects/main/game/notc/server/eth/exec.cs
@@ -5,38 +5,6 @@ function executeEthernetServerScripts()
 {
    exec("notc/server/base/exec.cs");
 
-   exec("notc/server/client/gui/BeepMsgHud/v1/exec.cs");
-   exec("notc/server/client/gui/MinimapHud/v1/exec.cs");
-   exec("notc/server/client/gui/LoadoutHud/v1/exec.cs");
-   
-   exec("notc/server/forms/etherform/exec.cs");
-   exec("notc/server/forms/standardcat/exec.cs");
-   exec("notc/server/forms/snipercat/exec.cs");
-
-   exec("notc/server/items/damper/exec.cs");
-   exec("notc/server/items/vamp/exec.cs");
-   exec("notc/server/items/impshield/exec.cs");
-   exec("notc/server/items/ballast/exec.cs");
-   exec("notc/server/items/stealth/exec.cs");
-   exec("notc/server/items/etherboard/exec.cs");
-   exec("notc/server/items/launcher/exec.cs");
-   exec("notc/server/items/bounce/exec.cs");
-   
-   exec("notc/server/items/g1launcher/v1/exec.cs");
-
-   exec("notc/server/weapons/razordisc/v1/exec.cs");
-   exec("notc/server/weapons/explosivedisc/v1/exec.cs");
-   exec("notc/server/weapons/repeldisc/v1/exec.cs");
-   exec("notc/server/weapons/interceptordisc/v1/exec.cs");
-   
-   exec("notc/server/weapons/smg1/v1/exec.cs");
-   exec("notc/server/weapons/mgl1/v1/exec.cs");
-   exec("notc/server/weapons/sg1/v1/exec.cs");
-   exec("notc/server/weapons/sg2/v1/exec.cs");
-   exec("notc/server/weapons/sr1/v1/exec.cs");
-   exec("notc/server/weapons/mg1/v1/exec.cs");
-   exec("notc/server/weapons/ml1/v1/exec.cs");
-
    exec("./aiPlayer.cs");
    exec("./commands.cs");
    exec("./eth.cs");
