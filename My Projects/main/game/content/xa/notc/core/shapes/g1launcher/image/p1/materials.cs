9be74eb764da62199be61f8191d7109fdf0f3b74
Michael Goldener <mg@wasted.ch>
2014-09-02 04:06:44 -0700

    Tweak p1 G1Launcher materials.

--- My Projects/main/game/content/xa/notc/core/shapes/g1launcher/image/p1/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/g1launcher/image/p1/materials.cs
@@ -2,12 +2,12 @@
 singleton Material(xa_notc_core_shapes_g1launcher_image_p1_mat1)
 {
    mapTo = "xa_notc_core_shapes_g1launcher_image_p1_mat1";
-   emissive[0] = "0";
+   emissive[0] = "1";
    glow[0] = "0";
    diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    diffuseColorPaletteSlot[0] = "0";
    diffuseMap[0] = "content/xa/notc/core/shapes/standardcat/armor.png";
-   pixelSpecular[0] = "1";
+   pixelSpecular[0] = "0";
    emissive[1] = "1";
    glow[1] = "1";
    diffuseColor[1] = "0.992157 0.992157 0.992157 1";
@@ -16,7 +16,7 @@ singleton Material(xa_notc_core_shapes_g1launcher_image_p1_mat1)
    diffuseColor[2] = "0.996078 0.996078 0.996078 1";
    diffuseColorPaletteSlot[2] = "0";
    diffuseMap[2] = "content/xa/notc/core/textures/grid128.12.png";
-   glow[2] = "1";
+   glow[2] = "0";
    emissive[2] = "1";
    wireframe[2] = "1";
    materialTag0 = "Miscellaneous";
