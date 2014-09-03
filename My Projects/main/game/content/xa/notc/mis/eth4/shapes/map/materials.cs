221b1f3786258a6122fe85fa7d4da55c8dc928df
Michael Goldener <mg@wasted.ch>
2014-09-02 21:39:11 -0700

    Work on ETH4 map.

--- My Projects/main/game/content/xa/notc/mis/eth4/shapes/map/materials.cs
+++ My Projects/main/game/content/xa/notc/mis/eth4/shapes/map/materials.cs
@@ -1,22 +1,21 @@
 
-singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat1)
+singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat2)
 {
-   mapTo = "xa_notc_mis_eth4_shapes_map_mat1";
+   mapTo = "xa_notc_mis_eth4_shapes_map_mat2";
    diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    specular[0] = "0.5 0.5 0.5 1";
    specularPower[0] = "50";
    translucentBlendOp = "None";
-   diffuseColor[1] = "0.996078 0.996078 0.996078 1";
-   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.4.png";
-   emissive[1] = "1";
-   detailScale[0] = "16 16";
+   diffuseColor[1] = "1 1 1 1";
+   emissive[1] = "0";
+   detailScale[0] = "2 2";
    detailNormalMapStrength[0] = "1";
-   diffuseMap[0] = "content/xa/notc/core/textures/white.128.png";
    pixelSpecular[0] = "0";
    materialTag0 = "Miscellaneous";
-   detailMap[0] = "content/xa/notc/core/textures/grid.128.4.png";
    diffuseColorPaletteSlot[0] = "0";
-   diffuseColorPaletteSlot[1] = "1";
+   diffuseColorPaletteSlot[1] = "2";
+   showDust = "1";
+   footstepSoundId = "2";
 };
 
 singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat2)
@@ -31,15 +30,25 @@ singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat2)
    materialTag0 = "Miscellaneous";
 };
 
-singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat2)
+singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat3)
 {
-   mapTo = "xa_notc_mis_eth4_shapes_map_mat2";
+   mapTo = "xa_notc_mis_eth4_shapes_map_mat3";
    diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    specular[0] = "0.5 0.5 0.5 1";
    specularPower[0] = "50";
    translucentBlendOp = "None";
    materialTag0 = "Miscellaneous";
-   diffuseColorPaletteSlot[0] = "0";
+   diffuseColorPaletteSlot[0] = "3";
+   diffuseColor[1] = "0.992157 0.992157 0.992157 1";
+   diffuseColor[2] = "0.262745 0.521569 0.482353 0";
+   diffuseColorPaletteSlot[1] = "4";
+   diffuseMap[0] = "content/xa/notc/core/textures/white.128.png";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.4.png";
+   detailScale[0] = "16 16";
+   emissive[1] = "1";
+   wireframe[2] = "1";
+   showDust = "1";
+   footstepSoundId = "2";
 };
 
 singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat3)
