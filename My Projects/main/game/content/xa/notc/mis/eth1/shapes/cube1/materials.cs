43b377f4f440b75f5e6457dd46a8301286cf983e
Michael Goldener <mg@wasted.ch>
2014-09-03 00:19:13 -0700

    Work on ETH1 map.

--- My Projects/main/game/content/xa/notc/mis/eth1/shapes/cube1/materials.cs
+++ My Projects/main/game/content/xa/notc/mis/eth1/shapes/cube1/materials.cs
@@ -20,23 +20,7 @@ singleton Material(xa_notc_mis_eth1_shapes_cube1_mat1)
    footstepSoundId = "2";
    impactSoundId = "-1";
    materialTag0 = "Miscellaneous";
+   glow[0] = "0";
 };
 
-singleton Material(shape_xa_notc_mis_eth1_shapes_cube1_mat1)
-{
-   mapTo = "xa_notc_mis_eth1_shapes_cube1_mat1";
-   diffuseColor[0] = "0.431373 0.431373 0.431373 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-   pixelSpecular[0] = "1";
-};
 
-singleton Material(shape_xa_notc_mis_eth1_shapes_cube1_mat2)
-{
-   mapTo = "xa_notc_mis_eth1_shapes_cube1_mat2";
-   diffuseColor[0] = "0.580392 0.00784314 0.996078 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-   glow[0] = "1";
-   emissive[0] = "1";
-};
