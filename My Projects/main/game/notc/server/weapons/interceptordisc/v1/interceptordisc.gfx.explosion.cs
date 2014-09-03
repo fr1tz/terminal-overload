2b321d615c891e59c8c49981846bf7fde27b77d8
Michael Goldener <mg@wasted.ch>
2014-09-03 00:55:18 -0700

    Tweak interceptor disc explosion visuals.

--- My Projects/main/game/notc/server/weapons/interceptordisc/v1/interceptordisc.gfx.explosion.cs
+++ My Projects/main/game/notc/server/weapons/interceptordisc/v1/interceptordisc.gfx.explosion.cs
@@ -1,7 +1,7 @@
 // Copyright information can be found in the file named COPYING
 // located in the root directory of this distribution.
 
-datablock ParticleData(WpnInterceptorDiscExplosion_Smoke)
+datablock ParticleData(WpnInterceptorDiscExplosion_Particle1)
 {
 	dragCoeffiecient	  = 0.4;
 	gravityCoefficient	= -0.4;
@@ -24,77 +24,35 @@ datablock ParticleData(WpnInterceptorDiscExplosion_Smoke)
 	allowLighting = false;
 };
 
-datablock ParticleEmitterData(WpnInterceptorDiscExplosion_SmokeEmitter)
+datablock ParticleEmitterData(WpnInterceptorDiscExplosion_ParticleEmitter1)
 {
 	ejectionOffset	= 0;
 
-	ejectionPeriodMS = 40;
+	ejectionPeriodMS = "1";
 	periodVarianceMS = 0;
 
-	ejectionVelocity = 2.0;
-	velocityVariance = 0.1;
+	ejectionVelocity = "15";
+	velocityVariance = "5";
 
-	thetaMin			= 0.0;
-	thetaMax			= 60.0;
+	thetaMin			= "0";
+	thetaMax			= "180";
 
-	lifetimeMS		 = 100;
+	lifetimeMS		 = "100";
 
 	particles = "WpnInterceptorDiscExplosion_Smoke";
-};
-
-datablock ParticleData(WpnInterceptorDiscExplosion_DebrisParticles)
-{
-	spinSpeed = 200;
-	spinRandomMin = -200.0;
-	spinRandomMax =  200.0;
-	dragCoefficient		= 1;
-	gravityCoefficient	= 2.5;
-	windCoefficient		= 0.0;
-	inheritedVelFactor	= 0.0;
-	constantAcceleration = 0.0;
-	lifetimeMS			  = 1500;
-	lifetimeVarianceMS	= 0;
-	textureName = "content/xa/rotc/p.5.4/shapes/rotc/misc/debris1.white";
-	colors[0]	  = "1.0 1.0 1.0 1.0";
-	colors[1]	  = "1.0 1.0 1.0 1.0";
-	colors[2]	  = "1.0 1.0 1.0 0.0";
-	sizes[0]		= 0.25;
-	sizes[1]		= 0.25;
-	sizes[2]		= 0.25;
-	times[0]		= 0.0;
-	times[1]		= 0.5;
-	times[2]		= 1.0;
-	useInvAlpha =  false;
-	allowLighting = false;
-};
-
-datablock ParticleEmitterData(WpnInterceptorDiscExplosion_DebrisEmitter)
-{
-	ejectionPeriodMS = 50;
-	periodVarianceMS = 0;
-	ejectionVelocity = 10.0;
-	velocityVariance = 5.0;
-	ejectionOffset	= 0.0;
-	thetaMin			= 0;
-	thetaMax			= 60;
-	phiReferenceVel  = 0;
-	phiVariance		= 360;
-	lifetimeMS		 = 5;
-	lifetimeVarianceMS = 0;
-	overrideAdvances = false;
-	orientParticles  = true;
-	particles = "WpnInterceptorDiscExplosion_DebrisParticles";
+   blendStyle = "ADDITIVE";
+   orientParticles = "1";
 };
 
 datablock ExplosionData(WpnInterceptorDiscExplosion)
 {
 	soundProfile = WpnInterceptorDiscExplosionSound;
 
-	explosionShape = "content/xa/rotc_hack/shapes/explosion_white.dts";
+	explosionShape = "content/xa/notc/core/shapes/interceptordisc/explosion/p1/shape.dts";
 	faceViewer	  = true;
 	playSpeed = 4.0;
-	sizes[0] = "0.2 0.2 0.2";
-	sizes[1] = "1.0 1.0 1.0";
+	sizes[0] = "4.0 4.0 4.0";
+	sizes[1] = "0.0 0.0 0.0";
 	times[0] = 0.0;
 	times[1] = 1.0;
 
@@ -105,7 +63,7 @@ datablock ExplosionData(WpnInterceptorDiscExplosion)
 //	debrisVelocity = 20.0;
 //	debrisVelocityVariance = 15.0;
 
-//	emitter[0] = Team1AntiairNearEnemyExplosion_CloudEmitter;
+	emitter[0] = WpnInterceptorDiscExplosion_ParticleEmitter1;
 //	emitter[0] = Team1AntiairNearEnemyExplosion_Cloud2Emitter;
 
 	// Camera Shaking
@@ -123,3 +81,22 @@ datablock ExplosionData(WpnInterceptorDiscExplosion)
 };
 
 
+
+datablock ParticleData(WpnInterceptorDiscExplosion_Smoke)
+{
+   gravityCoefficient = "1";
+   inheritedVelFactor = "0.025";
+   lifetimeMS = "500";
+   lifetimeVarianceMS = "200";
+   useInvAlpha = "1";
+   textureName = "content/xa/rotc/p.5.4/textures/rotc/spark02.png";
+   animTexName = "content/xa/rotc/p.5.4/textures/rotc/spark02.png";
+   colors[0] = "0.980392 0.980392 0.980392 1";
+   colors[1] = "1 1 1 0";
+   times[1] = "1";
+   times[2] = "1";
+   allowLighting = "0";
+   dragCoeffiecient = "0.4";
+   sizes[0] = "2";
+   sizes[1] = "2";
+};
