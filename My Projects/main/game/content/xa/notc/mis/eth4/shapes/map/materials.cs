eb3e65450355cfbbb7ad44c2cd262933f693dc56
Michael Goldener <mg@wasted.ch>
2014-09-20 09:14:52 -0700

    Work on ETH4 map.

--- My Projects/main/game/content/xa/notc/mis/eth4/shapes/map/materials.cs
+++ My Projects/main/game/content/xa/notc/mis/eth4/shapes/map/materials.cs
@@ -2,21 +2,21 @@
 singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat1)
 {
    mapTo = "xa_notc_mis_eth4_shapes_map_mat1";
-   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
+   diffuseColor[0] = "0.431373 0.431373 0.431373 1";
    specular[0] = "0.5 0.5 0.5 1";
    specularPower[0] = "50";
    translucentBlendOp = "None";
    diffuseColor[1] = "0.996078 0.996078 0.996078 1";
    diffuseMap[1] = "content/xa/notc/core/textures/grid.128.4.png";
    emissive[1] = "1";
-   detailScale[0] = "16 16";
+   detailScale[0] = "1 1";
    detailNormalMapStrength[0] = "1";
    diffuseMap[0] = "content/xa/notc/core/textures/white.128.png";
    pixelSpecular[0] = "0";
    materialTag0 = "Miscellaneous";
-   detailMap[0] = "content/xa/notc/core/textures/grid.128.4.png";
    diffuseColorPaletteSlot[0] = "0";
    diffuseColorPaletteSlot[1] = "1";
+   detailMap[0] = "content/xa/rotc/p.5.4/textures/malloc/detail.grid.png";
 };
 
 singleton Material(struct1_xa_notc_mis_eth4_shapes_map_mat2)
