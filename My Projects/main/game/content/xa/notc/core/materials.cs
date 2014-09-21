7caa4ca091432da60cc31ce037862c789635e453
Michael Goldener <mg@wasted.ch>
2014-09-20 21:37:12 -0700

    Add universal core material: xa_notc_core_wireframemat.

---
+++ My Projects/main/game/content/xa/notc/core/materials.cs
@@ -0,0 +1,18 @@
+
+singleton Material(xa_notc_core_wireframemat)
+{
+   mapTo = "xa_notc_core_wireframemat";
+   diffuseColor[0] = "0.992157 0.996078 0.996078 1";
+   diffuseColorPaletteSlot[0] = "0";
+   doubleSided = "0";
+   translucentBlendOp = "None";
+   glow[0] = "0";
+   emissive[0] = "1";
+   castShadows = "0";
+   translucent = "0";
+   wireframe[0] = "1";
+   materialTag0 = "Miscellaneous";
+   alphaRef = "0";
+   showFootprints = "0";
+};
+
