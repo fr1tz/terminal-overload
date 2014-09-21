99e1a85b26fb54035cb0b470331a8a9f025d274f
Michael Goldener <mg@wasted.ch>
2014-09-20 21:40:32 -0700

    Change StandardCAT visuals.

--- My Projects/main/game/content/xa/notc/core/shapes/standardcat/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/standardcat/materials.cs
@@ -66,36 +66,33 @@ singleton Material(xa_notc_core_shapes_standardcat_trailsmat)
 singleton Material(xa_notc_core_shapes_standardcat_mat1)
 {
    mapTo = "base";
-   diffuseColor[0] = "1 1 1 1";
-   diffuseColorPaletteSlot[0] = "1";
-   diffuseMap[0] = "content/xa/notc/core/shapes/standardcat/armor.png";
+   diffuseColor[0] = "0.415686 0.415686 0.415686 1";
+   diffuseColorPaletteSlot[0] = "-1";
    showFootprints = "0";
    materialTag0 = "Miscellaneous";
    doubleSided = "0";
    translucent = "0";
    translucentZWrite = "0";
    translucentBlendOp = "None";
-   emissive[0] = "1";
+   emissive[0] = "0";
    glow[0] = "0";
-   diffuseColor[1] = "0.988235 0.996078 0.996078 1";
-   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
-   diffuseColorPaletteSlot[1] = "1";
+   diffuseColor[1] = "0.988235 0.996078 0.996078 0";
+   diffuseColorPaletteSlot[1] = "-1";
    glow[1] = "0";
    wireframe[1] = "1";
-   emissive[1] = "1";
+   emissive[1] = "0";
+   specular[1] = "1 1 1 1";
 };
 
 singleton Material(xa_notc_core_shapes_standardcat_mat2)
 {
    mapTo = "armor_red0024";
-   emissive[0] = "1";
+   emissive[0] = "0";
    glow[0] = "0";
    diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    diffuseColorPaletteSlot[0] = "0";
-   diffuseMap[0] = "content/xa/notc/core/shapes/standardcat/armor.png";
    pixelSpecular[0] = "0";
-   diffuseColor[1] = "0.996078 0.996078 0.996078 1";
-   diffuseMap[1] = "content/xa/notc/core/textures/grid128.12.png";
+   diffuseColor[1] = "0.439216 0.439216 0.439216 1";
    diffuseColorPaletteSlot[1] = "0";
    glow[1] = "0";
    emissive[1] = "1";
@@ -107,7 +104,10 @@ singleton Material(xa_notc_core_shapes_standardcat_mat2)
    translucentBlendOp = "None";
    alphaRef = "0";
    showFootprints = "0";
-   pixelSpecular[1] = "1";
+   pixelSpecular[1] = "0";
+   specularPower[0] = "1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.8.png";
+   detailScale[0] = "16 16";
 };
 
 singleton Material(xa_notc_core_shapes_standardcat_stealthmat1)
