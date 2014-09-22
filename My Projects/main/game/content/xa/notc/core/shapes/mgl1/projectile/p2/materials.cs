652bb556fd380d6ae7706dd2bc0eab5d1408a9e2
Michael Goldener <mg@wasted.ch>
2014-09-21 23:39:14 -0700

    Work on p2 MGL1 projectile shape.

--- My Projects/main/game/content/xa/notc/core/shapes/mgl1/projectile/p2/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/mgl1/projectile/p2/materials.cs
@@ -9,7 +9,7 @@ singleton Material(xa_notc_core_shapes_mgl1_projectile_p2_mat1)
    glow[0] = "0";
    emissive[0] = "1";
    castShadows = "0";
-   translucent = "0";
+   translucent = "1";
    wireframe[0] = "0";
    materialTag0 = "Miscellaneous";
    diffuseColorPaletteSlot[1] = "0";
@@ -19,67 +19,3 @@ singleton Material(xa_notc_core_shapes_mgl1_projectile_p2_mat1)
    diffuseColor[1] = "0.964706 0.964706 0.964706 0";
 };
 
-
-singleton Material(shape_xa_notc_core_wireframemat)
-{
-   mapTo = "xa_notc_core_wireframemat";
-   diffuseColor[0] = "0.64 0.563579 0 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-};
-
-singleton Material(shape_xa_notc_core_shapes_mgl1_projectile_p2_mat1)
-{
-   mapTo = "xa_notc_core_shapes_mgl1_projectile_p2_mat1";
-   diffuseColor[0] = "0.64 0.563579 0 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-};
-
-singleton Material(shape_xa_notc_core_wireframemat)
-{
-   mapTo = "xa_notc_core_wireframemat";
-   diffuseColor[0] = "0.64 0.563579 0 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-};
-
-singleton Material(shape_xa_notc_core_shapes_mgl1_projectile_p2_mat1)
-{
-   mapTo = "xa_notc_core_shapes_mgl1_projectile_p2_mat1";
-   diffuseColor[0] = "0.64 0.563579 0 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-};
-
-singleton Material(shape_xa_notc_core_wireframemat)
-{
-   mapTo = "xa_notc_core_wireframemat";
-   diffuseColor[0] = "0.64 0.563579 0 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-};
-
-singleton Material(shape_xa_notc_core_shapes_mgl1_projectile_p2_mat1)
-{
-   mapTo = "xa_notc_core_shapes_mgl1_projectile_p2_mat1";
-   diffuseColor[0] = "0.64 0.563579 0 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-};
-
-singleton Material(shape_xa_notc_core_wireframemat)
-{
-   mapTo = "xa_notc_core_wireframemat";
-   diffuseColor[0] = "0.64 0.563579 0 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-};
-
-singleton Material(shape_xa_notc_core_shapes_mgl1_projectile_p2_mat1)
-{
-   mapTo = "xa_notc_core_shapes_mgl1_projectile_p2_mat1";
-   diffuseColor[0] = "0.64 0.563579 0 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-};
