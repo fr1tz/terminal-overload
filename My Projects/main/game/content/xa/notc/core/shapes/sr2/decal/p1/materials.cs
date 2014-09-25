7804b2222eac5a4adda32fc475e597b90636648a
Michael Goldener <mg@wasted.ch>
2014-09-25 10:33:51 -0700

    Add xa stuff for SR2.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/sr2/decal/p1/materials.cs
@@ -0,0 +1,18 @@
+
+singleton Material(xa_notc_core_shapes_sr2_decal_p1_mat0)
+{
+   diffuseMap[0] = "content/xa/notc/core/shapes/sr2/decal/p1/decal.png";
+   translucent = "1";
+   translucentBlendOp = "AddAlpha";
+   castShadows = "0";
+   showFootprints = "0";
+   glow[0] = "1";
+   emissive[0] = "1";
+   diffuseColor[0] = "0.996078 0.996078 0.992157 1";
+   translucentZWrite = "1";
+   vertColor[0] = "1";
+   materialTag0 = "Miscellaneous";
+   mapTo = "corona.png";
+};
+
+
---
+++ My Projects/main/game/content/xa/notc/core/shapes/sr2/explosion/p1/materials.cs
@@ -0,0 +1,20 @@
+
+
+singleton Material(xa_notc_core_shapes_sr2_explosion_p1_mat)
+{
+   mapTo = "xa_notc_core_shapes_sr2_explosion_p1_mat";
+   diffuseMap[0] = "content/xa/notc/core/shapes/sr1/explosion/p1/texture.png";
+   vertColor[0] = "1";
+   glow[0] = "1";
+   emissive[0] = "1";
+   doubleSided = "1";
+   castShadows = "0";
+   translucent = "1";
+   translucentBlendOp = "AddAlpha";
+   showFootprints = "0";
+   diffuseColorPaletteSlot[0] = "0";
+   glowOnly[0] = "0";
+   materialTag0 = "Miscellaneous";
+   effectColor[0] = "0 0 0 0";
+   diffuseColor[0] = "0.215686 0.215686 0.215686 1";
+};
