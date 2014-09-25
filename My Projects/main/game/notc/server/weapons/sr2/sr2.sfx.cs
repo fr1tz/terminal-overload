7eb4ff7cf8d25a61a0098c9ec9c5a2b0949da514
Michael Goldener <mg@wasted.ch>
2014-09-25 12:54:48 -0700

    Fix SR2 sounds.

--- My Projects/main/game/notc/server/weapons/sr2/sr2.sfx.cs
+++ My Projects/main/game/notc/server/weapons/sr2/sr2.sfx.cs
@@ -1,42 +1,42 @@
 // Copyright information can be found in the file named COPYING
 // located in the root directory of this distribution.
 
-datablock SFXProfile(WpnSR1FireSound)
+datablock SFXProfile(WpnSR2FireSound)
 {
    filename = "content/xa/rotc/p.5.4/sounds/rotc/explosion12";
    description = AudioClose3D;
    preload = true;
 };
 
-datablock SFXProfile(WpnSR1DryFireSound)
+datablock SFXProfile(WpnSR2DryFireSound)
 {
    filename = "content/xa/rotc/p.5.4/sounds/rotc/weaponEmpty";
    description = AudioClose3D;
    preload = true;
 };
 
-datablock SFXProfile(WpnSR1ChargeSound)
+datablock SFXProfile(WpnSR2ChargeSound)
 {
    filename = "content/xa/rotc/p.5.4/sounds/rotc/charge2";
    description = AudioClose3D;
    preload = true;
 };
 
-datablock SFXProfile(WpnSR1SwitchinSound)
+datablock SFXProfile(WpnSR2SwitchinSound)
 {
    filename = "content/xa/torque3d/3.0/sound/weapons/wpn_lurker_switchin";
    description = AudioClose3D;
    preload = true;
 };
 
-datablock SFXProfile(WpnSR1ProjectileImpactSound)
+datablock SFXProfile(WpnSR2ProjectileImpactSound)
 {
    filename = "content/xa/rotc/p.5.4/sounds/rotc/explosion2";
    description = AudioClose3D;
    preload = true;
 };
 
-datablock SFXProfile(WpnSR1ProjectileMissedEnemySound)
+datablock SFXProfile(WpnSR2ProjectileMissedEnemySound)
 {
    filename = "content/xa/rotc/p.5.4/sounds/rotc/impact4";
    description = AudioClose3D;
