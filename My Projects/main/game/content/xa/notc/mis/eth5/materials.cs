01fdbe90d7b4b119d1f5e17d0c615bf2b28789e0
Michael Goldener <mg@wasted.ch>
2014-10-04 03:38:40 -0700

    Work on ETH5 map.

--- My Projects/main/game/content/xa/notc/mis/eth5/materials.cs
+++ My Projects/main/game/content/xa/notc/mis/eth5/materials.cs
@@ -2,12 +2,42 @@ new TerrainMaterial()
 {
    detailDistance = "6000";
    internalName = "eth5";
-   detailMap = "content/xa/notc/mis/eth5/textures/grid";
+   detailMap = "content/xa/notc/mis/eth5/textures/xa_notc_mis_eth5_grid";
    diffuseSize = "4";
    detailSize = "4";
    detailStrength = "1";
    macroStrength = "0.1";
-   diffuseMap = "content/xa/notc/mis/eth5/textures/grid";
+   diffuseMap = "content/xa/notc/mis/eth5/textures/xa_notc_mis_eth5_grid";
+};
+
+singleton Material(xa_notc_mis_eth5_gridfx)
+{
+   mapTo = "xa_notc_mis_eth5_grid";
+   footstepSoundId = "2";
+   terrainMaterials = "1";
+   ShowDust = "true";
+   showFootprints = "1";
+   materialTag0 = "Terrain";
+   effectColor[0] = "0.00784314 0.94902 0.996078 1";
+   effectColor[1] = "0.996078 0.00784314 0.901961 1";
+   glow[0] = "0";
+   emissive[0] = "0";
+   detailScale[0] = "64 64";
+   scrollDir[0] = "0 -0.03";
+   scrollSpeed[0] = "1.647";
+   castShadows = "0";
+   translucentBlendOp = "None";
+   parallaxScale[0] = "1";
+   useAnisotropic[0] = "1";
+   diffuseColor[0] = "0.0235294 1 0 1";
+   diffuseMap[1] = "content/xa/notc/mis/dm1/textures/grid1.png";
+   glow[1] = "1";
+   emissive[1] = "1";
+   detailScale[2] = "64 64";
+   detailScale[1] = "64 64";
+   diffuseColor[2] = "0.00392157 0.00392157 0.00392157 1";
+   diffuseColor[1] = "0.992157 0.992157 0.992157 1";
+   impactSoundId = "2";
 };
 
 singleton Material(xa_notc_mis_eth5_territoryzone_terrainmat)
@@ -35,58 +65,10 @@ singleton Material(xa_notc_mis_eth5_territoryzone_terrainmat)
    translucentZWrite = "1";
    effectColor[0] = "InvisibleBlack";
 };
-singleton Material(xa_notc_mis_eth5_territoryzone_terrainmat)
-{
-   diffuseColor[0] = "1 1 1 1";
-   diffuseColorPaletteSlot[0] = "0";
-   doubleSided = "0";
-   translucentBlendOp = "LerpAlpha";
-   parallaxScale[0] = "0";
-   castShadows = "0";
-   translucent = "1";
-   showFootprints = "0";
-   glow[0] = "0";
-   emissive[0] = "0";
-   materialTag0 = "Miscellaneous";
-   animFlags[0] = "0x00000000";
-   mapTo = "zone.grid.png";
-   wireframe[0] = "0";
-   diffuseColorPaletteSlot[1] = "0";
-   glow[1] = "0";
-   emissive[1] = "0";
-   wireframe[1] = "0";
-   diffuseColor[1] = "1 1 1 0.25";
-   diffuseMap[0] = "content/xa/notc/mis/eth5/textures/territory";
-   translucentZWrite = "1";
-};
-singleton Material(xa_notc_mis_eth5_territoryzone_terrainmat)
-{
-   diffuseColor[0] = "1 1 1 1";
-   diffuseColorPaletteSlot[0] = "0";
-   doubleSided = "0";
-   translucentBlendOp = "LerpAlpha";
-   parallaxScale[0] = "0";
-   castShadows = "0";
-   translucent = "1";
-   showFootprints = "0";
-   glow[0] = "0";
-   emissive[0] = "0";
-   materialTag0 = "Miscellaneous";
-   animFlags[0] = "0x00000000";
-   mapTo = "zone.grid.png";
-   wireframe[0] = "0";
-   diffuseColorPaletteSlot[1] = "0";
-   glow[1] = "0";
-   emissive[1] = "0";
-   wireframe[1] = "0";
-   diffuseColor[1] = "1 1 1 0.25";
-   diffuseMap[0] = "content/xa/notc/mis/eth5/textures/territory";
-   translucentZWrite = "1";
-};
 
 singleton Material(xa_notc_mis_eth5_territoryzone_bordermat)
 {
-   diffuseColor[0] = "0.5 0.5 0.5 1";
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    diffuseColorPaletteSlot[0] = "1";
    doubleSided = "0";
    translucentBlendOp = "AddAlpha";
