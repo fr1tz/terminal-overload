4a590a8c0fec69c80e0acd721112552ba0059397
Michael Goldener <mg@wasted.ch>
2014-10-04 03:39:33 -0700

    ETH: Give class #1 razor discs instead of repel and explosive discs.

--- My Projects/main/game/notc/server/eth/eth.cs
+++ My Projects/main/game/notc/server/eth/eth.cs
@@ -292,15 +292,13 @@ function ETH::loadoutPlayer(%player)
 
    %player.setInventory(ItemG1Launcher, 1);
    
-   %player.setInventory(WpnRepelDiscAmmo, 9999);
-   %player.setInventory(WpnExplosiveDiscAmmo, 9999);
-   
    %player.clearWeaponCycle();
    switch(%client.zActiveLoadout)
    {
       case 0:
          %player.setInventory(ItemEtherboard, 1);
          %player.setInventory(ItemStealth, 1);
+         %player.setInventory(WpnRazorDiscAmmo, 9999);
          %player.setInventory(WpnSMG2, 1);
          %player.setInventory(WpnSG3, 1);
          %player.addToWeaponCycle(WpnSMG2);
@@ -309,16 +307,22 @@ function ETH::loadoutPlayer(%player)
       case 1:
          %player.setInventory(ItemEtherboard, 1);
          %player.setInventory(ItemStealth, 1);
+         %player.setInventory(WpnRepelDiscAmmo, 9999);
+         %player.setInventory(WpnExplosiveDiscAmmo, 9999);
          %player.setInventory(WpnMGL2, 1);
          %player.addToWeaponCycle(WpnMGL2);
          %player.mountImage(WpnMGL2Image, 0);
       case 2:
          %player.setInventory(ItemEtherboard, 1);
          %player.setInventory(ItemStealth, 1);
+         %player.setInventory(WpnRepelDiscAmmo, 9999);
+         %player.setInventory(WpnExplosiveDiscAmmo, 9999);
          %player.setInventory(WpnSR2, 1);
          %player.addToWeaponCycle(WpnSR2);
          %player.mountImage(WpnSR2Image, 0);
       case 3:
+         %player.setInventory(WpnRepelDiscAmmo, 9999);
+         %player.setInventory(WpnExplosiveDiscAmmo, 9999);
          %player.setInventory(WpnMG2, 1);
          %player.addToWeaponCycle(WpnMG2);
          %player.mountImage(WpnMG2Image, 0);
