b39d673e7c209cbfa2299339a48941500fd2c8d9
Michael Goldener <mg@wasted.ch>
2014-10-24 11:23:43 -0700

    Add console functions: getTime() and getDateAndTime().

--- Engine/source/console/consoleFunctions.cpp
+++ Engine/source/console/consoleFunctions.cpp
@@ -2560,6 +2560,47 @@ DefineEngineFunction( isToolBuild, bool, (),,
 #endif
 }
 
+//----------------------------------------------------------------
+
+ConsoleFunctionGroupBegin( DateAndTime, "Functions to access date & time" );
+
+ConsoleFunction( getTime, S32, 1, 1, "Returns current time" )
+{
+	return Platform::getTime();
+}
+
+ConsoleFunction( getDateAndTime, const char*, 1, 1, "Returns current date & time" )
+{
+	char* ret = Con::getReturnBuffer(512);
+	Platform::LocalTime lt;
+	Platform::getLocalTime(lt);
+	const char* month;
+	switch(lt.month) {
+		case 0: month = "Jan"; break;
+		case 1: month = "Feb"; break;
+		case 2: month = "Mar"; break;
+		case 3: month = "Apr"; break;
+		case 4: month = "May"; break;
+		case 5: month = "Jun"; break;
+		case 6: month = "Jul"; break;
+		case 7: month = "Aug"; break;
+		case 8: month = "Sep"; break;
+		case 9: month = "Oct"; break;
+		case 10: month = "Nov"; break;
+		case 11: month = "Dec"; break;
+	};
+	dSprintf(ret, 512, "%s.%d.%d-%02d:%02d:%02d",
+               month,
+               lt.monthday,
+               lt.year + 1900,
+               lt.hour,
+               lt.min,
+               lt.sec);
+	return ret;
+}
+
+ConsoleFunctionGroupEnd( DateAndTime );
+
 //-----------------------------------------------------------------------------
 
 ConsoleFunctionGroupBegin( Cryptography, "Cryptographic functions" );
