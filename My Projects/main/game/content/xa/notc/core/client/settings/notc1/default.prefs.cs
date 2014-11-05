c0a855ec7644128946fb40120b9459673cb45b32
Michael Goldener <mg@wasted.ch>
2014-11-05 03:42:15 -0800

    NOTC1 settings bugfix: Don't overwrite certain $Pref::* vars if they exist.

--- My Projects/main/game/content/xa/notc/core/client/settings/notc1/default.prefs.cs
+++ My Projects/main/game/content/xa/notc/core/client/settings/notc1/default.prefs.cs
@@ -1,7 +1,7 @@
 // Copyright information can be found in the file named COPYING
 // located in the root directory of this distribution.
 
-$Pref::NOTC1::DefaultFov = 80;
-$Pref::NOTC1::MouseZoomSteps = 3;
-$Pref::NOTC1::MouseInvertY = false;
+if($Pref::NOTC1::DefaultFov $= "") $Pref::NOTC1::DefaultFov = 80;
+if($Pref::NOTC1::MouseZoomSteps $= "") $Pref::NOTC1::MouseZoomSteps = 3;
+if($Pref::NOTC1::MouseInvertY $= "") $Pref::NOTC1::MouseInvertY = false;
 
