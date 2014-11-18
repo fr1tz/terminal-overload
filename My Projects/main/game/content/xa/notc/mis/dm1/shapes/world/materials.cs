f36d8d273eb196514ce85e832bd5103d982757fa
Michael Goldener <mg@wasted.ch>
2014-11-18 08:12:15 -0800

    DM1's world shape: Use palette slots.

--- My Projects/main/game/content/xa/notc/mis/dm1/shapes/world/materials.cs
+++ My Projects/main/game/content/xa/notc/mis/dm1/shapes/world/materials.cs
@@ -26,42 +26,55 @@ singleton Material(xa_notc_mis_dm1_shapes_world_outermat)
    translucent = "0";
    showFootprints = "0";
    cubemap = "xa_notc_core_cubemaps_sys1";
-};
-
-singleton Material(xa_notc_mis_dm1_shapes_world_mat3)
-{
-   mapTo = "xa_notc_mis_dm1_shapes_world_mat3";
-   diffuseColor[0] = "0.00784314 0.580392 0.996078 1";
-   doubleSided = "1";
-   translucentBlendOp = "None";
-   glow[0] = "1";
-   emissive[0] = "1";
+   specular[1] = "1 1 1 1";
    materialTag0 = "Miscellaneous";
 };
 
 singleton Material(xa_notc_mis_dm1_shapes_world_mat5)
 {
    mapTo = "xa_notc_mis_dm1_shapes_world_mat5";
-   diffuseColor[0] = "0 0 0 1";
+   diffuseColor[0] = "0.992157 0.996078 0.996078 1";
    doubleSided = "1";
    translucentBlendOp = "None";
    glow[0] = "0";
    emissive[0] = "0";
    materialTag0 = "Miscellaneous";
-   detailScale[0] = "96 96";
    diffuseColor[1] = "0.992157 0.992157 0.992157 1";
-   glow[1] = "1";
-   emissive[1] = "1";
-   detailMap[0] = "content/xa/notc/core/textures/grid.128.4.png";
-   wireframe[1] = "1";
    diffuseColor[2] = "0 1 0.894118 0";
+   diffuseColorPaletteSlot[0] = "0";
+   diffuseColorPaletteSlot[1] = "1";
+   detailScale[0] = "96 96";
+   glow[1] = "1";
    glow[2] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
 };
 
 singleton Material(shape_xa_notc_mis_dm1_shapes_world_mat6)
 {
    mapTo = "xa_notc_mis_dm1_shapes_world_mat6";
-   diffuseColor[0] = "0 0 0 1";
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+   glow[0] = "1";
+   emissive[0] = "0";
+   materialTag0 = "Miscellaneous";
+   detailScale[0] = "96 96";
+   diffuseColor[1] = "0.988235 0.996078 0.988235 1";
+   glow[1] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
+   diffuseColor[2] = "1 1 1 1";
+   glow[2] = "0";
+   diffuseColorPaletteSlot[0] = "0";
+   diffuseColorPaletteSlot[1] = "1";
+   diffuseMap[1] = "content/xa/notc/core/textures/grid.128.4.png";
+};
+
+singleton Material(shape_xa_notc_mis_dm1_shapes_world_mat6)
+{
+   mapTo = "xa_notc_mis_dm1_shapes_world_mat6";
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    doubleSided = "1";
    translucentBlendOp = "None";
    glow[0] = "1";
@@ -74,6 +87,8 @@ singleton Material(shape_xa_notc_mis_dm1_shapes_world_mat6)
    emissive[1] = "1";
    detailMap[0] = "content/xa/notc/core/textures/grid.128.4.png";
    wireframe[1] = "1";
+   diffuseColorPaletteSlot[0] = "0";
+   diffuseColorPaletteSlot[1] = "1";
 };
 
 singleton Material(shape_xa_notc_mis_dm1_shapes_world_mat7)
