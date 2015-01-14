938fe680afff0b526298f10e12a861dfde0cb878
Michael Goldener <mg@wasted.ch>
2015-01-14 18:59:50 +0100

    Add p1 flag shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/flag/p1/materials.cs
@@ -0,0 +1,17 @@
+
+singleton Material(xa_notc_core_shapes_flag_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_flag_p1_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.992157 1";
+   specular[3] = "White";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   diffuseColorPaletteSlot[0] = "0";
+   glow[0] = "1";
+   emissive[0] = "1";
+   diffuseColor[1] = "White";
+   castShadows = "0";
+   translucent = "1";
+};
+
+
