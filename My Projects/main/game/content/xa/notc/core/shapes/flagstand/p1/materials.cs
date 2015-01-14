f5cd37b848c258fc2f5801a33952cc8219e7fcf2
Michael Goldener <mg@wasted.ch>
2015-01-14 19:00:11 +0100

    Add p1 flagstand shape.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/flagstand/p1/materials.cs
@@ -0,0 +1,11 @@
+
+singleton Material(shape_xa_notc_core_shapes_flagstand_p1_mat1)
+{
+   mapTo = "xa_notc_core_shapes_flagstand_p1_mat1";
+   diffuseColor[0] = "0.996078 0.996078 0.992157 1";
+   translucentBlendOp = "None";
+   diffuseColorPaletteSlot[0] = "0";
+   diffuseColorPaletteSlot[1] = "0";
+   emissive[1] = "1";
+   wireframe[1] = "1";
+};
