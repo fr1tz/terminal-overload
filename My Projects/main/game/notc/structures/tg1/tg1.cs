7a21ac5969aafa5718df578df76a5205aa888f69
Michael Goldener <mg@wasted.ch>
2015-01-20 01:22:17 +0100

    Work on TerritoryGenerator1.

--- My Projects/main/game/notc/structures/tg1/tg1.cs
+++ My Projects/main/game/notc/structures/tg1/tg1.cs
@@ -22,6 +22,7 @@ function TerritoryGenerator1::onAdd(%this, %obj)
    %obj.zLastUpdatePosition = "";
    %obj.zUpdateAssetsThread = "";
    %obj.zTower = "";
+   %obj.zBeam = "";
    %obj.zZone = "";
    %this.updateAssetsThread(%obj);
 }
@@ -38,6 +39,9 @@ function TerritoryGenerator1::removeAssets(%this, %obj)
 
    if(isObject(%obj.zTower))
       %obj.zTower.delete();
+      
+   if(isObject(%obj.zBeam))
+      %obj.zBeam.delete();
 
    if(isObject(%obj.zZone))
       %obj.zZone.delete();
@@ -66,7 +70,7 @@ function TerritoryGenerator1::updateAssetsThread(%this, %obj)
    %this.removeAssets(%obj);
    
    %obj.zTower = new TSStatic() {
-      shapeName = "content/xa/notc/deathmatch/shapes/spawntower/p1/shape.dae";
+      shapeName = "content/xa/notc/core/shapes/tg1/p1/shape.dae";
       playAmbient = "1";
       meshCulling = "0";
       originSort = "0";
@@ -82,17 +86,34 @@ function TerritoryGenerator1::updateAssetsThread(%this, %obj)
       canSaveDynamicFields = "1";
    };
    
+   %obj.zBeam = new TSStatic() {
+      shapeName = "content/xa/notc/core/shapes/tg1/p1/beam.dae";
+      playAmbient = "1";
+      meshCulling = "0";
+      originSort = "0";
+      collisionType = "Collision Mesh";
+      decalType = "Collision Mesh";
+      allowPlayerStep = "1";
+      renderNormals = "0";
+      forceDetail = "-1";
+      position = %pos;
+      rotation = "1 0 0 0";
+      scale = "0.25 0.25 5000";
+      canSave = "1";
+      canSaveDynamicFields = "1";
+   };
+   
    %obj.zZone = new TacticalZone() {
       dataBlock = "TerritoryZone";
       client = "-1";
       teamId = %obj.teamId;
       position = %pos;
-      rotation = "0 0 1 45";
-      scale = "32 32 32";
+      rotation = "0 0 1 0";
+      scale = "48 48 32";
       canSave = "1";
       canSaveDynamicFields = "1";
       showOnMinimap = "1";
-      renderInteriors = "1";
+      renderInteriors = "0";
       renderTerrain = "1";
       renderTerrainGrid = "1";
       borderBottom = "2";
@@ -103,9 +124,21 @@ function TerritoryGenerator1::updateAssetsThread(%this, %obj)
       borderTop = "2";
    };
    
+   %obj.zZone.zGenerator = %obj;
+   %obj.zZone.zUnblockable = true;
    %obj.zZone.getDataBlock().setZoneOwner(%obj.zZone, %obj.getTeamId());
    copyPalette(%obj.zZone, %obj.zTower);
+   copyPalette(%obj.zZone, %obj.zBeam);
 
    %obj.zLastUpdatePosition = %obj.getPosition();
 }
 
+function TerritoryGenerator1::onPaletteChange(%this, %obj, %slot, %colorI)
+{
+   echo("TerritoryGenerator1::onPaletteChange()");
+   changePalette(%obj.zTower, %slot, %colorI);
+   changePalette(%obj.zBeam, %slot, %colorI);
+   changePalette(%obj.zZone, %slot, %colorI);
+   if(%slot == 0)
+      changePalette(%obj.zZone, 1, %colorI);
+}
