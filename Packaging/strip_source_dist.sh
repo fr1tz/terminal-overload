fb8711a892e36b3123e8cea3d488163cdbcc535d
Michael Goldener <mg@wasted.ch>
2015-01-29 02:56:39 +0100

    Work on packaging scripts.

--- Packaging/strip_source_dist.sh
+++ Packaging/strip_source_dist.sh
@@ -10,13 +10,14 @@ abort()
 strip_source_dist()
 (
 	echo "Stripping content not included in release from source dist..."
+	CONTENT=$(cat ./content.txt)
 	cd 'dists/source/My Projects/main/game/' || abort
 	( mv content content.tmp && mkdir content ) || abort
 	(
 		cd content.tmp || abort
-		for F in $(find -name INCLUDE_IN_RELEASE)
+		for F in "$CONTENT"
 		do
-			DIR=$(dirname $F) && cp --parent -r $DIR ../content
+			cp --parent -r $F ../content
 		done
 	)
 	rm -r content.tmp || abort
