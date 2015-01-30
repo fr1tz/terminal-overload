daf45e5d616a9fa2a57c84201959f8ede7bbca6a
Michael Goldener <mg@wasted.ch>
2015-01-30 03:40:05 +0100

    Work on v1 XaNotcLoadingGui module.

--- My Projects/main/game/content/xa/notc/core/client/gui/LoadingGui/v1/commands.cs
+++ My Projects/main/game/content/xa/notc/core/client/gui/LoadingGui/v1/commands.cs
@@ -1,21 +1,34 @@
 // Copyright information can be found in the file named COPYING
 // located in the root directory of this distribution.
 
-function clientCmd_XaNotcLoadingGui_SetLeftPanel(%text, %append)
+function clientCmd_XaNotcLoadingGui_SetBackground(%bitmap)
 {
-   //echo("clientCmd_XaNotcLoadingGui_SetLeftPanel()");
-   if(%append)
-      XaNotcLoadingGui-->LeftPanel.addText(%text);
+   //echo("clientCmd_XaNotcLoadingGui_SetBackground()");
+   if(isFile(%bitmap))
+   {
+      XaNotcLoadingGui-->bg.setBitmap(%bitmap);
+      XaNotcLoadingGui-->bg.setVisible(true);
+   }
    else
-      XaNotcLoadingGui-->LeftPanel.setText(%text);
+      XaNotcLoadingGui-->bg.setVisible(false);
 }
 
-function clientCmd_XaNotcLoadingGui_SetRightPanel(%text, %append)
+
+function clientCmd_XaNotcLoadingGui_SetLayer(%layer, %text, %append)
 {
-   //echo("clientCmd_XaNotcLoadingGui_SetRightPanel()");
+   //echo("clientCmd_XaNotcLoadingGui_SetLayer()");
+   switch(%layer)
+   {
+      case 1: %ctrl = XaNotcLoadingGui-->layer1;
+      case 2: %ctrl = XaNotcLoadingGui-->layer2;
+      case 3: %ctrl = XaNotcLoadingGui-->layer3;
+      default: return;
+   }
+   
    if(%append)
-      XaNotcLoadingGui-->RightPanel.addText(%text);
+      %ctrl.addText(%text);
    else
-      XaNotcLoadingGui-->RightPanel.setText(%text);
+      %ctrl.setText(%text);
 }
+
 
--- My Projects/main/game/content/xa/notc/core/client/gui/LoadingGui/v1/loadingGui.cs
+++ My Projects/main/game/content/xa/notc/core/client/gui/LoadingGui/v1/loadingGui.cs
@@ -4,29 +4,18 @@
 //------------------------------------------------------------------------------
 function XaNotcLoadingGui::onAdd(%this)
 {
-   %this.qLineCount = 0;
+
 }
 
 //------------------------------------------------------------------------------
 function XaNotcLoadingGui::onWake(%this)
 {
-   // Play sound...
-   //CloseMessagePopup();
+
 }
 
 //------------------------------------------------------------------------------
 function XaNotcLoadingGui::onSleep(%this)
 {
-   // Clear the load info:
-   if ( %this.qLineCount !$= "" )
-   {
-      for ( %line = 0; %line < %this.qLineCount; %line++ )
-         %this.qLine[%line] = "";
-   }      
-   %this.qLineCount = 0;
-
    XaNotcLoadingGui-->LoadingProgress.setValue( 0 );
    XaNotcLoadingGui-->LoadingText.setValue( "WAITING FOR SERVER" );
-
-   // Stop sound...
 }
