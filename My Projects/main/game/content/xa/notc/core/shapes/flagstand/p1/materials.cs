f3c9898f6d86995e7e48082875bf022f7888afd5
Michael Goldener <mg@wasted.ch>
2015-02-04 09:33:23 +0100

    Work on p1 flagstand shape.

--- My Projects/main/game/content/xa/notc/core/shapes/flagstand/p1/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/flagstand/p1/materials.cs
@@ -1,11 +1,20 @@
 
+
 singleton Material(shape_xa_notc_core_shapes_flagstand_p1_mat1)
 {
    mapTo = "xa_notc_core_shapes_flagstand_p1_mat1";
    diffuseColor[0] = "0.996078 0.996078 0.992157 1";
-   translucentBlendOp = "None";
+   translucentBlendOp = "AddAlpha";
+   diffuseColor[1] = "1 1 1 0";
    diffuseColorPaletteSlot[0] = "0";
    diffuseColorPaletteSlot[1] = "0";
+   glow[0] = "1";
+   emissive[0] = "1";
    emissive[1] = "1";
+   wireframe[0] = "1";
    wireframe[1] = "1";
+   doubleSided = "1";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
 };
