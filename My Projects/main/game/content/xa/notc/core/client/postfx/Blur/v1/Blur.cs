942f0b57281e6e7b2d03e522c70e9bacb0cad51e
Michael Goldener <mg@wasted.ch>
2015-03-07 11:20:36 +0100

    Add NotcBlurPostFx.

---
+++ My Projects/main/game/content/xa/notc/core/client/postfx/Blur/v1/Blur.cs
@@ -0,0 +1,29 @@
+// Copyright information can be found in the file named COPYING
+// located in the root directory of this distribution.
+
+singleton PostEffect( NotcBlurPostFx )
+{
+   renderTime = "PFXAfterDiffuse";
+   renderPriority = 0.2;
+   isEnabled = false;
+   allowReflectPass = false;
+   
+   shader = PFX_PassthruShader;
+   stateBlock = PFX_DefaultStateBlock;
+   texture[0] = "$backBuffer";
+   target = "$outTex";
+   targetScale = "1.0 1.0";
+   
+   new PostEffect()
+   {
+      shader = PFX_PassthruShader;
+      stateBlock = PFX_DefaultStateBlock;
+      texture[0] = "$inTex";
+      target = "$backBuffer";
+   };
+};
+
+function NotcBlurPostFx::setShaderConsts( %this )
+{
+
+}
---
+++ My Projects/main/game/content/xa/notc/core/client/postfx/Blur/v1/exec.cs
@@ -0,0 +1,9 @@
+// Copyright information can be found in the file named COPYING
+// located in the root directory of this distribution.
+
+function loadContentModule_NotcBlurPostFx()
+{
+   exec("./Blur.cs");
+}
+
+loadContentModule_NotcBlurPostFx();
