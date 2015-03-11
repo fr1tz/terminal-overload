c34884ff410bee91238772c806542659eb8d8534
Michael Goldener <mg@wasted.ch>
2015-03-11 08:22:17 +0100

    Bump game version to "0.7.0-dev1".

--- My Projects/main/game/version.cs
+++ My Projects/main/game/version.cs
@@ -1,8 +1,8 @@
-$GameVersionString = "0.6.0+dev";
+$GameVersionString = "0.7.0-dev1";
 
 // Version Type. Either "dev", "testing" or "public"
 $GameVersionType = "dev";
 
 // Version Feedback URL (Optional, without "http://").
-$GameVersionFeedbackURL = "";
+$GameVersionFeedbackURL = "forum.freegamedev.net/viewtopic.php?f=88&t=6204";
 
