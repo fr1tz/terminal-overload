a99e2e2311280f2437fa9051b6ff5b75f2a30098
Michael Goldener <mg@wasted.ch>
2015-03-11 06:28:07 +0100

    Add $GameVersionType (dev,testing,public) and $GameVersionFeedbackURL vars.

--- My Projects/main/game/version.cs
+++ My Projects/main/game/version.cs
@@ -1,1 +1,8 @@
 $GameVersionString = "0.6.0+dev";
+
+// Version Type. Either "dev", "testing" or "public"
+$GameVersionType = "dev";
+
+// Version Feedback URL (Optional, without "http://").
+$GameVersionFeedbackURL = "";
+
