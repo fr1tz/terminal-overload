1f9e1ef8a333a5deee3fe169ac60957245aaa5c1
Michael Goldener <mg@wasted.ch>
2015-04-03 13:38:44 +0200

    Tweak p1 MGL2 materials.

--- My Projects/main/game/content/xa/notc/core/shapes/mgl2/image/p1/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/mgl2/image/p1/materials.cs
@@ -3,35 +3,36 @@
 singleton Material(notc_core_shapes_mgl2_image_p1_shellmat1)
 {
    mapTo = "xa_notc_core_shapes_mgl2_image_p1_mat1";
-   diffuseColor[0] = "0.156863 0.156863 0.156863 1";
+   diffuseColor[0] = "0.00392157 0.00392157 0.00392157 1";
    specular[0] = "0.5 0.5 0.5 1";
    specularPower[0] = "50";
    doubleSided = "0";
-   translucentBlendOp = "AddAlpha";
+   translucentBlendOp = "None";
    detailScale[0] = "4 4";
    emissive[0] = "1";
    castShadows = "0";
-   translucent = "1";
+   translucent = "0";
    showFootprints = "0";
    diffuseColorPaletteSlot[0] = "-1";
    specular[3] = "1 1 1 1";
    materialTag0 = "Miscellaneous";
+   accuScale[0] = "0.03125";
 };
 
 singleton Material(notc_core_shapes_mgl2_image_p1_shellmat2)
 {
    mapTo = "xa_notc_core_shapes_mgl2_image_p1_mat2";
-   diffuseColor[0] = "0.470588 0.470588 0.470588 1";
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
    specular[0] = "0.5 0.5 0.5 1";
    specularPower[0] = "50";
    translucentBlendOp = "None";
-   diffuseColorPaletteSlot[0] = "-1";
+   diffuseColorPaletteSlot[0] = "0";
    emissive[0] = "1";
-   wireframe[0] = "0";
+   wireframe[0] = "1";
    materialTag0 = "Miscellaneous";
    translucent = "0";
    castShadows = "0";
    showFootprints = "0";
-   glow[0] = "0";
+   glow[0] = "1";
 };
 
