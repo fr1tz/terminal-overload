58834d7f94bf8268427fff379d4dc8078eb0b3b8
Michael Goldener <mg@wasted.ch>
2015-04-04 18:27:52 +0200

    Add materials for SG3 laser trail.

---
+++ My Projects/main/game/content/xa/notc/core/shapes/sg3/trail/p1/materials.cs
@@ -0,0 +1,64 @@
+singleton Material(xa_notc_core_shapes_sg3_trail_p1_lasertrail0mat0)
+{
+   diffuseColor[0] = "0.996078 0.996078 0.996078 1";
+   doubleSided = "1";
+   translucentBlendOp = "AddAlpha";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   glow[0] = "1";
+   glowOnly[0] = "0";
+   emissive[0] = "1";
+   vertColor[0] = "1";
+   materialTag0 = "Miscellaneous";
+   wireframe[0] = "1";
+   diffuseColor[1] = "0.996078 0.996078 0.996078 0";
+   vertColor[1] = "1";
+   glow[1] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
+   diffuseColorPaletteSlot[0] = "0";
+};
+
+singleton Material(xa_notc_core_shapes_sg3_trail_p1_lasertrail0mat1)
+{
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
+   doubleSided = "0";
+   translucentBlendOp = "LerpAlpha";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   glow[0] = "1";
+   glowOnly[0] = "0";
+   emissive[0] = "1";
+   vertColor[0] = "1";
+   materialTag0 = "Miscellaneous";
+   diffuseColorPaletteSlot[0] = "-1";
+   mapTo = "laser1.white.png";
+   wireframe[0] = "0";
+   diffuseColor[3] = "1 1 1 1";
+   diffuseColor[1] = "0.996078 0.992157 0.992157 0";
+   diffuseColorPaletteSlot[1] = "0";
+   glow[1] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
+   specular[3] = "1 1 1 1";
+};
+
+singleton Material(xa_notc_core_shapes_sg3_trail_p1_lasertrail0mat2)
+{
+   diffuseColor[0] = "0.992157 0.992157 0.992157 1";
+   doubleSided = "0";
+   translucentBlendOp = "AddAlpha";
+   castShadows = "0";
+   translucent = "1";
+   showFootprints = "0";
+   glow[0] = "1";
+   glowOnly[0] = "1";
+   emissive[0] = "1";
+   vertColor[0] = "1";
+   materialTag0 = "Miscellaneous";
+   diffuseColorPaletteSlot[0] = "0";
+   mapTo = "laser1.white.png";
+};
+
