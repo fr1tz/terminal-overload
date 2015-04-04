fb34145f1c47904f7d84c0006fc125b4cca87998
Michael Goldener <mg@wasted.ch>
2015-04-04 17:30:30 +0200

    Tweak p1 MGL1 impact damage shape.

--- My Projects/main/game/content/xa/notc/core/shapes/mgl1/impactdmg/p1/materials.cs
+++ My Projects/main/game/content/xa/notc/core/shapes/mgl1/impactdmg/p1/materials.cs
@@ -10,10 +10,11 @@ singleton Material(xa_notc_core_shapes_mgl1_impactdmg_p1_mat1)
    emissive[0] = "1";
    castShadows = "0";
    showFootprints = "0";
-   glow[0] = "0";
+   glow[0] = "1";
    wireframe[0] = "0";
-   diffuseColor[1] = "0.996078 0.996078 0.996078 0";
-   glow[1] = "0";
+   diffuseColor[1] = "0.996078 0.956863 0.184314 1";
+   glow[1] = "1";
    wireframe[1] = "1";
+   emissive[1] = "1";
 };
 
