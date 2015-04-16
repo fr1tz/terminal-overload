cd0cebdb9695730dc055c3e7f75709ecb74723e2
Michael Goldener <mg@wasted.ch>
2015-04-16 21:15:30 +0200

    Update game version to 0.7.0-dev2.

--- My Projects/main/game/version.cs
+++ My Projects/main/game/version.cs
@@ -1,8 +1,8 @@
-$GameVersionString = "0.7.0-dev1+dev";
+$GameVersionString = "0.7.0-dev2";
 
 // Version Type. Either "dev", "testing" or "public"
 $GameVersionType = "dev";
 
 // Version Feedback URL (Optional, without "http://").
-$GameVersionFeedbackURL = "";
+$GameVersionFeedbackURL = "forum.freegamedev.net/viewtopic.php?f=88&t=6280";
 
