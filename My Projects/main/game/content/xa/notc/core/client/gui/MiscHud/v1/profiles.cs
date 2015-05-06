9948e67c69881e2ece075476ed554ad38bd2603b
Michael Goldener <mg@wasted.ch>
2015-05-07 00:39:43 +0200

    Work on MiscHud.

---
+++ My Projects/main/game/content/xa/notc/core/client/gui/MiscHud/v1/profiles.cs
@@ -0,0 +1,48 @@
+// Copyright information can be found in the file named COPYING
+// located in the root directory of this distribution.
+
+singleton GuiControlProfile(notcMiscHudImpulseDamperProfile)
+{
+   opaque = false;
+   fontType = "Quantico";
+   fontSize = 12;
+   fontColor = "0 255 255";
+   fillColor = "0 255 255";
+};
+
+singleton GuiControlProfile(notcMiscHudDamageDamperProfile)
+{
+   opaque = false;
+   fontType = "Quantico";
+   fontSize = 12;
+   fontColor = "255 0 255";
+   fillColor = "255 0 255";
+};
+
+singleton GuiControlProfile(notcMiscHudDamageBufferProfile)
+{
+   opaque = false;
+   fontType = "Quantico";
+   fontSize = 12;
+   fontColor = "255 255 255";
+   fillColor = "255 255 255";
+};
+
+singleton GuiControlProfile(notcMiscHudHealthProfile)
+{
+   opaque = false;
+   fontType = "Quantico";
+   fontSize = 12;
+   fontColor = "255 255 0";
+   fillColor = "255 255 0";
+};
+
+singleton GuiControlProfile(notcMiscHudBlackProfile)
+{
+   opaque = false;
+   fontType = "Quantico";
+   fontSize = 12;
+   fontColor = "0 0 0";
+   fillColor = "0 0 0";
+};
+
