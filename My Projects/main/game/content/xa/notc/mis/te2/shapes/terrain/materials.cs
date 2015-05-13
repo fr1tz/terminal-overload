e61703ba0d1b80234f98a547440e2d1d0ca30508
Michael Goldener <mg@wasted.ch>
2015-05-13 15:09:44 +0200

    Start work on TE2 map.

---
+++ My Projects/main/game/content/xa/notc/mis/te2/shapes/terrain/materials.cs
@@ -0,0 +1,43 @@
+
+
+singleton Material(shape_notc_mis_te2_shapes_terrain_mat1)
+{
+   mapTo = "notc_mis_te2_shapes_terrain_mat1";
+   diffuseColor[0] = "0.00784314 0.533333 0.996078 1";
+   specular[0] = "0.00784314 0.533333 0.996078 1";
+   specularPower[0] = "25";
+   translucentBlendOp = "AddAlpha";
+   diffuseColor[1] = "0.92549 0.996078 0.00784314 0";
+   glow[1] = "0";
+   emissive[1] = "0";
+   wireframe[1] = "0";
+   diffuseMap[0] = "content/xa/notc/mis/te2/shapes/terrain/noise.png";
+   materialTag0 = "Miscellaneous";
+   animFlags[0] = "0x00000008";
+   scrollDir[0] = "0.1 0.1";
+   scrollSpeed[0] = "10";
+   effectColor[1] = "InvisibleBlack";
+   pixelSpecular[0] = "1";
+   specular[1] = "1 1 1 1";
+   cubemap = "xa_notc_core_cubemaps_sys1";
+   rotSpeed[0] = "0.01";
+   waveAmp[0] = "1";
+   effectColor[0] = "0 0 0 0";
+   rotPivotOffset[0] = "-0.53 -0.57";
+};
+
+singleton Material(shape_notc_mis_te2_shapes_terrain_mat2)
+{
+   mapTo = "notc_mis_te2_shapes_terrain_mat2";
+   diffuseColor[0] = "0.00784314 0.533333 0.996078 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+   diffuseColor[1] = "0.027451 0.470588 0.00392157 0";
+   glow[0] = "1";
+   emissive[0] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
+   materialTag0 = "Miscellaneous";
+   wireframe[0] = "1";
+};
