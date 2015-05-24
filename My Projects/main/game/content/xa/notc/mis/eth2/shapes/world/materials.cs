1f0482fa2375777c8050be96dcdf1c39b54066ee
Michael Goldener <mg@wasted.ch>
2015-05-24 23:55:45 +0200

    Start work on ETH2 map.

---
+++ My Projects/main/game/content/xa/notc/mis/eth2/shapes/world/materials.cs
@@ -0,0 +1,70 @@
+
+
+singleton Material(notc_mis_eth2_shapes_world_mat1)
+{
+   mapTo = "notc_mis_eth2_shapes_world_mat1";
+   diffuseColor[0] = "0.00784314 0.533333 0.996078 1";
+   specular[0] = "0.00784314 0.533333 0.996078 1";
+   specularPower[0] = "25";
+   translucentBlendOp = "AddAlpha";
+   diffuseColor[1] = "0.00784314 0.972549 0.996078 0";
+   glow[1] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "0";
+   diffuseMap[0] = "content/xa/notc/mis/te2/shapes/terrain/noise.png";
+   materialTag0 = "Miscellaneous";
+   animFlags[0] = "0x00000008";
+   scrollDir[0] = "0.1 0.1";
+   scrollSpeed[0] = "10";
+   effectColor[1] = "InvisibleBlack";
+   pixelSpecular[0] = "1";
+   specular[1] = "1 1 1 1";
+   cubemap = "xa_notc_core_cubemaps_sys1";
+   rotSpeed[0] = "0.01";
+   waveAmp[0] = "1";
+   effectColor[0] = "InvisibleBlack";
+   rotPivotOffset[0] = "-0.53 -0.57";
+   detailScale[1] = "24 24";
+};
+
+singleton Material(notc_mis_eth2_shapes_world_mat2)
+{
+   mapTo = "notc_mis_eth2_shapes_world_mat2";
+   diffuseColor[0] = "0.2 0.976471 0.996078 0.091";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "AddAlpha";
+   diffuseColor[1] = "0.027451 0.470588 0.00392157 0";
+   glow[0] = "0";
+   emissive[0] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
+   materialTag0 = "Miscellaneous";
+   wireframe[0] = "1";
+   castShadows = "0";
+   showFootprints = "0";
+   translucent = "1";
+};
+
+singleton Material(notc_mis_eth2_shapes_world_mat3)
+{
+   mapTo = "notc_mis_eth2_shapes_world_mat3";
+   diffuseColor[0] = "0.227451 0.972549 0.992157 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "AddAlpha";
+   diffuseColor[1] = "0.027451 0.470588 0.00392157 0";
+   glow[0] = "0";
+   emissive[0] = "1";
+   emissive[1] = "1";
+   wireframe[1] = "1";
+   materialTag0 = "Miscellaneous";
+   wireframe[0] = "0";
+   castShadows = "0";
+   showFootprints = "0";
+   translucent = "1";
+   diffuseMap[0] = "content/xa/notc/mis/eth2/shapes/world/gradient1.png";
+   specular[3] = "1 1 1 1";
+};
+
+
